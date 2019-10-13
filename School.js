1bc676f0122716df01212a14f91e1e17568be6c2
Alejo <alejandrofranco@pursuit.org>
2019-10-13 01:18:06 -0400

    remove methods to get students failing and by city instead combine

--- School.js
+++ School.js
@@ -4,7 +4,8 @@ const Student = require('./Student')
 class School {
   constructor() {
     this.classes = {
-      //physics: {} Class Object
+      // className: Class Object
+      //   physics: {} 
     }
   }
 
@@ -27,7 +28,9 @@ class School {
    * @param {Student} student - Student object
    * @return {Student} Enrolled student
    */
-  enrollStudent(className, student) { }
+  enrollStudent(className, student) {
+    // Your code here
+  }
 
 
 
@@ -38,31 +41,31 @@ class School {
    * @param {string} className - Name of the class
    * @return {Student[]} Array of Student objects
    */
-  getStudentsByClass(className) { }
+  getStudentsByClass(className) {
+    // Your code here
+  }
 
 
 
 
   /**
-   * Get all students that are failing the class, 
-   * that is all students who's grade is less than 70
+   * Get all students and apply filters. If failing = true
+   * return all students that are failing the class, 
+   * that is all students whose grade is less than 70.
+   * If a city is passed return students whose city match
+   * the city passed. If both failing and city are passed
+   * return students that are failing and that live in the
+   * specified city
    * 
    * @param {string} className - Name of the class
+   * @param {boolean} failing - Whether to return students that are failing the class or not
+   * @param {string} city - Name of the city to match against students
    * @return {Student[]} Array of Student objects
    */
-  getFailingStudents(className) { }
+  getStudentsByClassWithFilter(className, failing, city) {
+    // Your code here
+  }
 
-
-
-
-  /**
-   * Get all students unique from all classes that 
-   * live in a specific city
-   *  
-   * @param {string} city - Name of the city
-   * @return {Student[]} Array of Student objects
-   */
-  getStudentsByCity(city) { }
 }
 
 module.exports = School;
