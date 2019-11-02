70f214d3f88fd2697c79dbb76fad1413ec883acb
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-02 03:14:47 -0400

    0.7, front-end init

---
+++ client/index.js
@@ -0,0 +1,14 @@
+/*
+Joseph P. Pasaoa
+Front-end JS | Express Web Server Project
+*/
+
+
+/* CUSTOM HELPERS */
+const log = console.log;
+
+
+/* POST DOM LOAD EXECS */
+document.addEventListener("DOMContentLoaded", () => {
+
+});
