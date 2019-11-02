761056d062c4f3621a8960aaf850f2a138b5e6d7
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-02 13:10:17 -0400

    0.9, communication sys

--- client/index.js
+++ client/index.js
@@ -4,11 +4,44 @@ Front-end JS | Express Web Server Project
 */
 
 
-/* CUSTOM HELPERS */
+/* HELPERS */
 const log = console.log;
+
+const clearStage = () => {
+  const stage = document.querySelector(`#response`);
+  while (stage.firstChild) {
+    stage.removeChild(stage.lastChild);
+  }
+}
+
+const serverComm = async (method, urlAdds, body) => {
+  const url = `http://localhost:11000/${urlAdds}`;
+  try {
+    const response = await axios[method](url);
+    log(response.data);
+    return response.data;
+  } catch(err) {
+    log("client-side error: ", err);
+  }
+}
+
+const showResponse = (result) => {
+  clearStage();
+
+  log("result: ", result);
+  log("jsoned: ", JSON.stringify(result, null, 2));
+  const stage = document.querySelector(`#response`);
+  setTimeout(() => { // gives more natural delay to displaying response
+      stage.innerHTML = JSON.stringify(result, null, 3);
+  }, 125);
+}
 
 
 /* POST DOM LOAD EXECS */
 document.addEventListener("DOMContentLoaded", () => {
-
+  document.querySelector('#buttonForm1').addEventListener("click", async (e) => {
+      const received = await serverComm("get", "school");
+      const responseStage = document.querySelector('#response');
+      showResponse(received);
+  });
 });
