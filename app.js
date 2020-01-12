e70bc784dfa0d43092bfc384ad82e3efb6e793fa
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-12 17:44:30 -0500

    add a post for /get

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const bodyParse = require("body-parser");
 
-const Class = require("./classes/Class.js");
-const Student = require("./classes/Student.js");
 const School = require("./classes/School.js");
-const classRouter = require("./routes/class/class.js");
 
 const port = 3000;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use("/class", classRouter);
-
+app.use(bodyParser.json()); 
 
 let mySchool = new School();
+
+app.post("/class", (req, res) => {
+    mySchool.addClass(req.body.name, req.body.teacher);
+})
 
 app.listen(port, () => console.log("Listening on port", port));
