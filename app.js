a13bc5558096b753f0ecd8c1e16a6a6ce1677321
maria liza <mariamartinez@pursuit.org>
2020-01-12 17:34:48 -0500

    add server

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = 3001
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+let mySchool = new School();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("Listening on port " + port)
+})
