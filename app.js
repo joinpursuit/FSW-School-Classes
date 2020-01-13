83f46bd46d0f60aec4bf859a238d3afbcbc5e0a0
Kevin <kevinwong@pursuit.org>
2020-01-12 21:23:31 -0500

    start to build backend server

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const port = 3000;
+const app = express();
+app.use(cors());
+
+app.listen(port, () => console.log("Listening on port: ", port));
