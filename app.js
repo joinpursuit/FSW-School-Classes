e2e3fd4fdf01e3d61bd296198be500dea4391de7
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-13 15:50:03 -0500

    add comments to app.js, and initialize html

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(bodyParser.json());
 let mySchool = new School();
 
 app.post("/class", (req, res) => {
+    // addClass returns false if the information is incorrect/class exists
+    // Checking if addClass returns false, if it does an error is sent
     if(!mySchool.addClass(req.body.name.toLowerCase(), req.body.teacher.toLowerCase())) {
         res.json({
             error: "Please fill out all the information or Class already exists",
@@ -31,6 +33,8 @@ app.post("/class", (req, res) => {
 app.post("/class/:className/enroll", (req, res) => {
     let userClass = req.params.className.toLowerCase();
 
+    // enrollStudent returns false if student information is incorrect
+    // Checking if false is returned, if it is then an error is sent
     if(!mySchool.enrollStudent(userClass, req.body)) {
         res.json({
             error: "Please fill out all the information for the student",
@@ -49,6 +53,7 @@ app.post("/class/:className/enroll", (req, res) => {
 app.get("/class/:className/students", (req, res) => {
     let currClass = req.params.className.toLowerCase();
     
+    // TODO: use getStudentByClassWithFilter method on mySchool. Args will be queries from the Front end
     if(mySchool.classes[currClass]) {
         res.json({
             students: mySchool.classes[currClass].students,
