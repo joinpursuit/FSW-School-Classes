89c64cf88d29bb31b9cad36548d17816a12b8dd7
John Jones Jr <jayjohnjones@gmail.com>
2020-01-13 21:32:36 -0500

    added npm installs to server file

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ const port = 3000;
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 let myschool = new School();
+
+
+app.listen(port, () => {
+    console.log("listening on port: ", port)
+});
+
+
+
