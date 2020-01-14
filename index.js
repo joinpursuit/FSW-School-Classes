38e05b8ecee75ac0840e8c85750d82fc7b2dbd36
maria liza <mariamartinez@pursuit.org>
2020-01-14 14:23:51 -0500

    add html elements to js and some more styling

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello")
+// console.log("hello")
+
+let formDiv = document.querySelector("formDiv")
+
+let formAddClass = document.querySelector("formAddClass")
+let teacherInput = document.querySelector("teacherInput")
+let classInput = document.querySelector("classInput")
+let addClassInfoBtn = document.querySelector("addClassInfo")
+
+
+let formAddStudent = document.querySelector("formAddStudent")
+let studentClassInput = document.querySelector("studentClassInput");
+let studentNameInput = document.querySelector("studentNameInput");
+let studentAgeInput = document.querySelector("studentAgeInput");
+let studentCityInput = document.querySelector("studentCityInput");
+let studentGradeInput = document.querySelectorid("studentGradeInput");
+let addStudentInfoBtn = document.querySelector("addStudentInfo");
+
+
+let formListStudents = document.querySelector("listStudents");
+let studentByClassInput = document.querySelector("studentByClassInput");
+let studentByCityInput = document.querySelector("studentByCityInput");
+let getStudentInfoBtn = document.querySelector("getStudentInfo"); 
+
