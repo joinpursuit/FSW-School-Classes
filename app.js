d936432ecee9ae2a7f5f9542b6cea7aa99d08e3b
Jay Fowler <jcafowler@gmail.com>
2020-01-13 19:13:14 -0500

    Ignore node modules

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const port = 3000
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`)
+})
