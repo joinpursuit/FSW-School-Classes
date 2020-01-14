11246d4821cad048c70c316c56bda0b01df709bf
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-13 19:48:19 -0500

    adding objects to use and test routes

--- app.js
+++ app.js
@@ -7,11 +7,25 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(bodyParser.json())
+
 const classRouter = require("./routes/classes.js")
 const studentRouter = require("./routes/students.js")
+const Class = require("./Class.js")
+const Student = require("./Student.js")
+const School = require("./School.js")
 
 app.use("/classes", classRouter)
 app.use("/students", studentRouter)
+app.use("Class",Class)
+app.use("Student",Student)
+app.use("School",School)
+
+let mySchool = new School();
+let newCourse = new Class("Javascript", "Ladovsky")
+console.log(newCourse)
+let deja = new Student("Deja", "30", "Brooklyn", "99")
+console.log(deja)
+
 
 app.listen(port, ()=>{
     console.log(`server is running at: ${port}` )
