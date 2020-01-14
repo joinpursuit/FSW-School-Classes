60b5d61e7a34c873cb526a67e5a00bc8b1aa8a5a
Phillip <phil.awich@gmail.com>
2020-01-13 21:29:52 -0500

    Add html/css files

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express();
 
 // let mySchool = new mySchool();
 
+app.get("/", (req,res) =>{
+    res.json("Hello School")
+})
+
+app.get("/school/courses", (req,res) => {
+    res.json([1,2,3])
+})
+
 app.listen(port, () => {
     console.log("App is listening on port ", port)
 })
