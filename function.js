4f5a216c8f56784e4e9c97527d06026946d6b2ee
John Jones Jr <jayjohnjones@gmail.com>
2020-01-14 21:58:29 -0500

    added to function.js

--- function.js
+++ function.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    let input = document.querySelector("input")
+    let input = document.querySelector("input2")
+    let input = document.querySelector("input3")
+    let input = document.querySelector("input4")
+    let input = document.querySelector("input5")
+    let input = document.querySelector("input6")
 })
