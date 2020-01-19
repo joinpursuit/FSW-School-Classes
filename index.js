1311d561dd30837eb82a66b1ba94f8851afd0249
your name here <your_email_here>
2020-01-19 17:46:06 -0500

    add dblclick hidden the output

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ document.addEventListener("DOMContentLoaded", async() => {
     classOpt(school.data)
     
     let schoolBtn = document.querySelector("#showAllClasses")
-    schoolBtn.addEventListener("click",async()=>{
+    schoolBtn.addEventListener("click",async(el)=>{
+        document.getElementById("showing").hidden=false
         let newSchool = await axios.get(url+"/");
         show(newSchool.data)
+    }
+    )
+    schoolBtn.addEventListener("dblclick",()=>{
+    document.getElementById("showing").hidden=true
     })
     
     let addClass = document.querySelector("#addClass")
@@ -25,20 +30,25 @@ document.addEventListener("DOMContentLoaded", async() => {
         classOpt(school.data)
     });
     
-
+    
     let enroll=document.querySelector("#addStudent")
     enroll.addEventListener("submit",async(el)=>{
         el.preventDefault();
-        await axios.post(`http://localhost:3000/school/add/student/${el.target.elements[0].value}`, {name: el.target.elements[1].value, age: el.target.elements[2].value, city:el.target.elements[3].value, grade:el.target.elements[4].value})
+        let addStudent =await axios.post(`http://localhost:3000/school/add/student/${el.target.elements[0].value}`, {name: el.target.elements[1].value, age: el.target.elements[2].value, city:el.target.elements[3].value, grade:el.target.elements[4].value})
+        console.log(addStudent.data)
     }
     )
     
-    let showStudent=document.querySelector("#showStudent")
-    showStudent.addEventListener("submit",async(el)=>{
-        el.preventDefault();
-        showStudentList(school.data,el.target.elements[0].value)
+    let showStudent=document.querySelector("#studentList")
+    showStudent.addEventListener("click",async(el)=>{
+        document.getElementById("showingStudent").hidden=false
+        school = await axios.get(url+"/");
+        showStudentList(school.data,el.target.form.elements[0].value)
     })
 
+    showStudent.addEventListener("dblclick",()=>{
+        document.getElementById("showingStudent").hidden=true
+    })
 })
 
 
@@ -48,17 +58,17 @@ const classOpt = (list)=>{
     classesList2.innerHTML="";
     classesList.innerHTML="";
     for(let key in list){
-       let classesOption=document.createElement("option")
-       classesOption.value=list[key].name;
-       classesOption.innerText=list[key].name;
-       classesList.appendChild(classesOption);
+        let classesOption=document.createElement("option")
+        classesOption.value=list[key].name;
+        classesOption.innerText=list[key].name;
+        classesList.appendChild(classesOption);
     };
-
+    
     for(let key in list){
-       let classesOption=document.createElement("option")
-       classesOption.value=list[key].name 
-       classesOption.innerText=list[key].name 
-       classesList2.appendChild(classesOption)
+        let classesOption=document.createElement("option")
+        classesOption.value=list[key].name 
+        classesOption.innerText=list[key].name 
+        classesList2.appendChild(classesOption)
     }
 }
 
@@ -69,14 +79,15 @@ const show = (data)=>{
         for(let el of data[key].students){
             element+="<p>"+"student: ["+el.name+ ", age: "+ el.age+", city :"+el.city+", grade :"+el.grade+"] </p>"
         }
-    document.getElementById("showing").innerHTML=""
-    document.getElementById("showing").innerHTML=element
+        document.getElementById("showing").innerHTML=""
+        document.getElementById("showing").innerHTML=element
+    }
 }
-}
+
 const showStudentList = (data,classSel)=>{
     let element = "";
-        for(let el of data[classSel].students){
-            element+="<p>"+"student: ["+el.name+ ", age: "+ el.age+", city :"+el.city+", grade :"+el.grade+"] </p>"
-        }
+    for(let el of data[classSel].students){
+        element+="<p>"+"student: ["+el.name+ ", age: "+ el.age+", city :"+el.city+", grade :"+el.grade+"] </p>"
+    }
     document.getElementById("showingStudent").innerHTML=element
 }
