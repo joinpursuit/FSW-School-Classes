c73507bce9daa9332a5161b105359c9130c42753
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-21 11:22:23 -0500

    add todo.js

---
+++ todo.js
@@ -0,0 +1,30 @@
+//     TODO: PIPEDREAM add a home page for a login (if student, teacher, or administrator) -->
+//     If no login then a signup is required
+//     Signup requires name and id match
+//     Setup with emaili and password
+
+//     Teachers takes to a page that allows you to change student grades
+//     Default has a list of students in the class that gets RT updated with changes
+
+//     Students takes to a page that allows you to enroll in classes and update your info
+//     Default has student info (including classes and grades), and gets RT updated with changes
+//     Maybe a seperate page for classes and grades
+
+//     Administrator takes to a page that allows you to enroll students in the school and new classes
+//     No default showing cause lists may be too long
+//     A page for all classes, and a seperate one for all students
+
+//     TODO: After setting up database for mySchool change the enroll student form to
+//     a drop down select of all classes and teachers (For list students too)
+//     Adds the ability to have multiple classes of the same name, with seperate teachers
+//     Maybe add a functionality to see/post teacher reviews
+
+//     TODO: Add in a new section to update the students information and not the grade
+//     the grade and class enrollment will have a different section
+//     This will require a student id (so using the find specific student is rec.)
+//     This will move list students section to a second row
+//     list students will take up the last 3 columns in row 2 (not the right most one)
+
+//     TODO: Add a find specific student section (takes in a name, and last name OR an ID)
+//     This will show the students information (ID, name, city, age, classes enrolled in and grades)
+//     Find Student will be in the first column (not the left most one)
