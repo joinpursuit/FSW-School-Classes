cca930a75eabe83eebb67687ec2ffac741fe9ccb
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-21 10:44:03 -0500

    clean up files, and functions

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const pgp = require("pg-promise")({});
 const connectionString = "postgres://localhost:5432/my_school_database";
 const db = pgp(connectionString);
 
-const School = require("./classes/School.js");
-
 const port = 3000;
 const app = express();
 
@@ -14,38 +12,56 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json()); 
 
-let mySchool = new School();
-
 const isClassExisting = async (newClass) => {
-    let classes;
-    if(typeof newClass === "string") {
-        classes = await db.any(`SELECT * FROM classes WHERE class_name=$1`, newClass);
-    } else {
-        classes = await db.any('SELECT * FROM classes WHERE class_name=${name} ' +
-                               'AND teacher=${teacher}', newClass);
+    try {
+        let classes;
+
+        // If just a class name is entered (as opposed to an object) then the first is ran
+        // Then it finds a class matching the given class name
+        if(typeof newClass === "string") {
+            classes = await db.any(`SELECT * FROM classes WHERE class_name=$1`, newClass);
+        } else {
+            classes = await db.any('SELECT * FROM classes WHERE class_name=${name}', newClass);
+        }
+        
+        if(classes.length) {
+            return true;
+        } else {
+            return false
+        }
+    } catch (err) {
+        console.log(err);
     }
     
-    if(classes.length) {
-        return true;
-    } else {
-        return false
-    }
-}
+} // End of isClassExisting() function
 
 const isStudentExisting = async (firstName, lastName) => {
-    let students =  await db.any('SELECT * FROM students WHERE first_name=$1 AND last_name=$2', [firstName, lastName]);
+    try {
+        // Finding any students that match the given name and last name
+        let students =  await db.any('SELECT * FROM students ' + 
+                                     'WHERE first_name=$1 AND last_name=$2', [firstName, lastName]);
 
-    if(students.length) {
-        return true;
-    } else {
-        return false;
+        if(students.length) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.log(err);
     }
-}
+    
+} // End of isStudentExisting() function
 
 const isStudentEnrolled = async (student, wantedClass) => {
     try {
-        let addedStudent = await db.any('SELECT * FROM students WHERE first_name=${firstName} AND last_name=${lastName}', student);
-        let enrolled =  await db.any('SELECT * FROM students INNER JOIN class_enrollments ON students.id=class_enrollments.student_id WHERE class_id=$1 AND student_id=$2',[wantedClass.id, addedStudent.id]);
+        // Find the existing student
+        let addedStudent = await db.any('SELECT * FROM students WHERE first_name=${firstName} AND ' +
+                                        'last_name=${lastName}', student);
+
+        // Check if the student is enrolled in the given class
+        let enrolled =  await db.any('SELECT * FROM students INNER JOIN class_enrollments ON ' +
+                                     'students.id=class_enrollments.student_id WHERE class_id=$1 AND ' + 
+                                     'student_id=$2',[wantedClass.id, addedStudent.id]);
         
         if(enrolled.length) {
             return true;
@@ -57,68 +73,118 @@ const isStudentEnrolled = async (student, wantedClass) => {
         console.log(err);
     }
     
-}
+} // End of isStudentEnrolled() function
 
 const findClass = async (className) => {
-    return await db.one('SELECT * FROM classes WHERE class_name=$1', className);
-}
+    try {
+        // Find the class matching the given name
+        return await db.one('SELECT * FROM classes WHERE class_name=$1', className);
+    } catch (err) {
+        console.log(err);
+    }
+    
+} // End of findClass() function
 
 const getStudentsByClass = async (className) => {
-    let {id} = await db.one('SELECT id FROM classes WHERE class_name=$1', className);
-    if(id) {
-        return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON students.id=class_enrollments.student_id WHERE class_id=$1', id);
-    } else {
-        return false;
-    }
-}
+    try {
+        // Grabbing the needed class ID from the given class 
+        let {id} = await db.one('SELECT id FROM classes WHERE class_name=$1', className);
 
-const getStudentsByClassWithFilter = async(className, failing, city) => {
-    let {id} = await db.one('SELECT id FROM classes WHERE class_name=$1', className);
-    if(id) {
-        if(failing && city) {
-            return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON students.id=class_enrollments.student_id WHERE class_id=$1 AND grade<70 AND city=$2', [id, city]);
-        } else if(failing) {
-            return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON students.id=class_enrollments.student_id WHERE class_id=$1 AND grade<70', id);
-        } else if(city) {
-            return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON students.id=class_enrollments.student_id WHERE class_id=$1 AND city=$2', [id, city]);
+        // If the class exists, and an ID is returned then find the students from that class
+        if(id) {
+            return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON ' + 
+                                'students.id=class_enrollments.student_id WHERE class_id=$1', id);
+        } else {
+            return false;
         }
-    } else {
-        return false;
+    } catch (err) {
+        console.log(err);
     }
-}
+   
+} // End of getStudentsByClass() function
+
+const getStudentsByClassWithFilter = async (className, failing, city) => {
+    try {
+        // Grabbing the needed ID from the given class
+        let {id} = await db.one('SELECT id FROM classes WHERE class_name=$1', className);
+
+        // If the class exists, and an ID is returned then find the students from that class (off filters)
+        if(id) {
+            if(failing && city) {
+                // Find the students from the given city that are failing
+                return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON ' + 
+                                    'students.id=class_enrollments.student_id WHERE class_id=$1 ' + 
+                                    'AND grade<70 AND city=$2', [id, city]);
+            } else if(failing) {
+                // Find the students that are failing
+                return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON ' + 
+                                    'students.id=class_enrollments.student_id WHERE class_id=$1 ' + 
+                                    'AND grade<70', id);
+            } else if(city) {
+                // Find the students from the given city
+                return await db.any('SELECT * FROM students INNER JOIN class_enrollments ON ' + 
+                                    'students.id=class_enrollments.student_id WHERE class_id=$1 ' + 
+                                    'AND city=$2', [id, city]);
+            }
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+    
+} // End of getStudentByClassWithFilter() function
 
 app.post("/class", async (req, res) => {
-    // addClass returns false if the information is incorrect/class exists
-    // Checking if addClass returns false, if it does an error is sent
     let newClass = req.body;
 
+    // Check ig the class is already existing
     if(await isClassExisting(newClass)) {
         res.status (400).json({
             error: "Class already exists",
             "timestamp": new Date()
-        })
+        });
+
     } else {
-        db.none('INSERT INTO classes(class_name, teacher) VALUES(${name}, ${teacher})', newClass).then(() => {
-            res.status (200).json({
-                class: req.body,
-                message: "created a new class",
-                timestamp: new Date().toString()
-            })
+        // Add the new class to the DB
+        db.none('INSERT INTO classes(class_name, teacher) ' + 
+                'VALUES(${name}, ${teacher})', newClass);
+
+        res.status (200).json({
+            class: req.body,
+            message: "created a new class",
+            timestamp: new Date().toString()
         })
     }
-})
+}) // End of /class route
 
 app.post("/class/:className/enroll", async (req, res) => {
     let className = req.params.className;
     req.body.name = req.body.firstName + " " + req.body.lastName;
     let student = req.body;
 
+    // Check if class is existing
     if(await isClassExisting(className)) {
        let newClass = await findClass(className);
        
-       if(await isStudentEnrolled(student, newClass)) {
-            let existingStd = await db.one('SELECT id FROM students WHERE first_name=${firstName} AND last_name=${lastName}', student);
-            db.none('UPDATE class_enrollments SET grade=$1 WHERE student_id=$2', [student.grade, existingStd.id]);
+       // Check if the student is Existing in the school
+       if(await isStudentExisting(student.firstName, student.lastName)) {
+           // If Yes then enroll the student in only the course
+            let existingStd = await db.one('SELECT id FROM students WHERE first_name=$firstName AND ' + 
+                                           'last_name=$lastName', student);
+
+            db.none('INSERT INTO class_enrollments (class_id, student_id) ' + 
+                    'VALUES($1, $2)', [newClass.id, existingStd.id]);
+
+        // Check if the student is enrolled in the class
+       } else if(await isStudentEnrolled(student, newClass)) {
+           // If yes then update the students info in the class (grade)
+
+            let existingStd = await db.one('SELECT id FROM students WHERE first_name=${firstName} AND ' + 
+                                           'last_name=${lastName}', student);
+
+            db.none('UPDATE class_enrollments SET grade=$1 ' + 
+                    'WHERE student_id=$2', [student.grade, existingStd.id]);
 
             res.json({ 
                 student: req.body,
@@ -126,12 +192,15 @@ app.post("/class/:className/enroll", async (req, res) => {
                 message: "Updated Student",
                 timestamp: new Date().toString()
             })
-       } else if(await isStudentExisting(student.firstName, student.lastName)) {
-            let existingStd = await db.one('SELECT id FROM students WHERE first_name=$firstName AND last_name=$lastName', student);
-            db.none('INSERT INTO class_enrollments (class_id, student_id) VALUES($1, $2)', [newClass.id, existingStd.id])
+
        } else {
-            let addedStudent = await db.one('INSERT INTO students (first_name, last_name, city, age) VALUES(${firstName}, ${lastName}, ${city}, ${age}) RETURNING *', student);
-            db.none('INSERT INTO class_enrollments (class_id, student_id, grade) VALUES($1, $2, $3)', [newClass.id, addedStudent.id, student.grade])
+           // If the student isn't existing then enroll in both the school AND the class
+
+            let added = await db.one('INSERT INTO students (first_name, last_name, city, age) ' + 
+                                     'VALUES(${firstName}, ${lastName}, ${city}, ${age}) RETURNING *', student);
+
+            db.none('INSERT INTO class_enrollments (class_id, student_id, grade) ' + 
+                    'VALUES($1, $2, $3)', [newClass.id, added.id, student.grade])
        }
 
         res.json({ 
@@ -147,22 +216,22 @@ app.post("/class/:className/enroll", async (req, res) => {
             timestamp: new Date().toString()
         })
     }
-
-    // enrollStudent returns false if student information is incorrect
-    // Checking if false is returned, if it is then an error is sent
-
-    
-})
+}) // End of /class/:className/enroll route
 
 app.get("/class/:className/students", async (req, res) => {
     let className = req.params.className;
     let {city, failing} = req.query;
     
+    // If class is existing then find the students
     if(await isClassExisting(className)) {
         let students;
+
+        // Check if either filters are passed
         if(city || failing) {
+            // Get the students by the entered filter
             students = await getStudentsByClassWithFilter(className, failing, city);
         } else {
+            // Get all students
             students = await getStudentsByClass(className);
         }
 
@@ -170,15 +239,17 @@ app.get("/class/:className/students", async (req, res) => {
             // If city or failing queries are passed, then WithFilter version of getStudentsByClass runs
             // Otherwise the normal version runs
             students: students,
+            className: className,
             message: "Retrieved students",
             timestamp: new Date().toString()
         }) 
+
     } else {
         res.json({
             error: `Class ${className} doesn't exist.`,
             timestamp: new Date().toString()
         })
     }
-})
+}) // End of /class/:className/students route
 
 app.listen(port, () => console.log("Listening on port", port));
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         listStudents()
     });
-})
+}) // End of DOMContentLoaded
 
 const postData = async (url, data, callback) => {
     try {
@@ -31,36 +31,44 @@ const postData = async (url, data, callback) => {
     } catch (err) {
         console.log(err);
     }
-}
+} // End of postData() function
 
 const addClass = async () => {
     let className = document.querySelector("#className");
     let classTeacher = document.querySelector("#classTeacher");
     let classSection = document.querySelector("#classResponse");
+    // Grab the needed class tags
 
+    // Check if either of the inputs are empty
     if(!className.value || !classTeacher.value) { 
+        // If true then add an error display
         classSection.innerHTML = "";
         let p = document.createElement("p");
         p.innerText = "Please fill out all information.";
         classSection.appendChild(p);
     } else {
+        // If all data was entered then post it to the DB
         let newClass = {name: className.value, teacher: classTeacher.value};
         postData("http://localhost:3000/class", newClass, appendClassResponse);
     }
     
-}
+} // End of addClass() function
 
 const appendClassResponse = (data) => {
     let classSection = document.querySelector("#classResponse");
     classSection.innerHTML = "";
+    // Reset the classSection section
 
     let newClass = data.class;
 
+    // Check if there is an error
     if(data.error) {
+        // Display an error if so
         let err = document.createElement("p");
         err.innerText = "Class already exists.";
         classSection.appendChild(err);
     } else {
+        // Display the information of the class added if no error
         let status = document.createElement("p");
         status.innerText = "Class Added";
         let p = document.createElement("p");
@@ -68,10 +76,11 @@ const appendClassResponse = (data) => {
         classSection.appendChild(status);
         classSection.appendChild(p);
 
+        // Reset the input tags
         document.querySelector("#className").value = "";
         document.querySelector("#classTeacher").value = "";
     }
-}
+} // End of appendClassResponse() function
 
 const addStudent = () => {
     let studentClass = document.querySelector("#studentClass");
@@ -81,13 +90,17 @@ const addStudent = () => {
     let studentCity = document.querySelector("#studentCity");
     let studentGrade = document.querySelector("#studentGrade");
     let studentSection = document.querySelector("#studentResponse");
+    // Grab all needed student tags
 
+    // Check if any info was empty
     if(!studentClass.value || !studentFirst.value || !studentLast.value || !studentAge.value || !studentCity.value || !studentGrade.value){
+        // If any inputs are empty then display an error
         studentSection.innerHTML = "";
         let p = document.createElement("p");
         p.innerText = "Please fill out all information.";
         studentSection.appendChild(p);
     } else {
+        // Post the data if all data is entered
         let student = {
             firstName: studentFirst.value,
             lastName: studentLast.value,
@@ -98,26 +111,42 @@ const addStudent = () => {
         
         postData(`http://localhost:3000/class/${studentClass.value}/enroll`, student, appendStdResponse);
     }
-}
+} // End of addStudent() function
 
 const appendStdResponse = (data) => {
     let studentSection = document.querySelector("#studentResponse");
     studentSection.innerHTML = "";
+    // Reset the studentSection
 
     let student = data.student;
 
+    // Check if for an error
     if(data.error) {
+        // Display it if so
         let err = document.createElement("p");
         err.innerText = data.error;
         studentSection.appendChild(err);
-    } else {
+    } else if(data.message === "Enrolled Student") {
+        // If student was fully enrolled then display
         let status = document.createElement("p");
         status.innerText = "Student Enrolled";
+        studentSection.appendChild(status);
+
+    } else if(data.message === "Updated Student") {
+        // If student info was update then display 
+
+        let status = document.createElement("p");
+        status.innerText = "Student Updated";
+        studentSection.appendChild(status);
+    }
+
+    if(!data.error) {
+        // Display info for the student
         let p = document.createElement("p");
         p.innerHTML = `<b>Name</b>: ${student.name} <b>Age</b>: ${student.age} <b>City</b>: ${student.city} <b>Grade</b>: ${student.grade}`;
-        studentSection.appendChild(status);
         studentSection.appendChild(p);
 
+        // Reset the input tags
         document.querySelector("#studentClass").value = "";
         document.querySelector("#studentFirst").value = "";
         document.querySelector("#studentLast").value = "";
@@ -125,15 +154,18 @@ const appendStdResponse = (data) => {
         document.querySelector("#studentAge").value = "";
         document.querySelector("#studentGrade").value = "";
     }
-}
+} // End of appendStdResponse() function
 
 const listStudents = async () => {
     let listClass = document.querySelector("#listClass");
     let listCity = document.querySelector("#listCity");
     let listFailing = document.querySelector("#listFailing");
     let listResponse = document.querySelector("#listResponse");
+    // Grab all needed list tags
 
+    // Check if no class was entered
     if(!listClass.value) {
+        // Display an enter class error if so
         listResponse.innerHTML = "";
         let p = document.createElement("p");
         p.innerText = "Please enter a class.";
@@ -141,6 +173,7 @@ const listStudents = async () => {
     } else {
         let res;
     
+        // Depending on the if the user wants failing students, students from a city or both get diff data
         if(listFailing.checked && listCity.value) {
             res = await axios.get(`http://localhost:3000/class/${listClass.value}/students?failing=true&city=${listCity.value}`);
         } else if(listFailing.checked) {
@@ -151,30 +184,40 @@ const listStudents = async () => {
             res = await axios.get(`http://localhost:3000/class/${listClass.value}/students`);
         }
 
+        // Reset input tags
         listClass.value = "";
         listCity.value = "";
         listFailing.checked = false;
 
+        // Post list data
         appendListResponse(res.data);
     }
-}
+} // End of listStudents() function
 
 const appendListResponse = (data) => {
-    debugger;
-    let students = data.students;
     let listResponse = document.querySelector("#listResponse");
     listResponse.innerHTML = "";
+    let students = data.students;
+    // Reset listResponse section and grab data.student
+
+    // Check if any students are returned
     if(!students) {
+        // If not then display "No students found"
         let noStudents = document.createElement("p");
         noStudents.innerText = "No students found.";
         listResponse.appendChild(noStudents);
     } else {
+        // If yes then display each student
         let studentList = document.createElement("ul");
+        let classHeading = document.createElement("h3");
+        classHeading.innerText = data.className;
+
         students.forEach(student => {
             let li = document.createElement("li");
             li.innerHTML = `<b>Name</b>: ${student.first_name} ${student.last_name} <b>Age</b>: ${student.age} <b>City</b>: ${student.city} <b>Grade</b>: ${student.grade}`;
             studentList.appendChild(li);
         })
+        listResponse.appendChild(classHeading);
         listResponse.appendChild(studentList);
     }
-}
+} // End of appendListResponse() function
