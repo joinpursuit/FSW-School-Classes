6999a3a67789ed2eab64cfeaa43b87e819334cf7
Henrynunez112 <henrynunez@pursuit.org>
2020-01-21 21:11:30 -0500

    working on the back-end

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(bodypaser.urlencoded({extended: false}));
 app.use(bodypaser.json());
 
 const School = require("./School.js");
+const Student = require("./Student.js");
+
 
 let mySchool = new School();
 
@@ -16,8 +18,17 @@ app.get("/", (req, res)=>{
 })
 
 app.post("/class", (req, res)=>{
-    let newClass = mySchool.addClass(req.body.name, req.body.teacher);
-    res.json(newClass);
+    if(!req.body.name || req.body.name === mySchool.classes){
+        res.json(
+            {"error": "Please fill out all the information or Class already exists",
+            "timestamp": "lol"
+        })
+    }else{
+        let newClass = mySchool.addClass(req.body.name, req.body.teacher);
+        res.json(newClass);
+    }
+    console.log(mySchool.classes)
+
 });
 app.post("/class/:className/enroll", (req, res)=>{
     let newStudent = mySchool.
