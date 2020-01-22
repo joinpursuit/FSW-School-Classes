fdcb2206ca2315141c8aba76e04a2fdc37008ca4
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-21 23:29:26 -0500

    add app.js file. download modules, check local host on postman, start front end layout with css.

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const app = express()
+const port = 3000;
+//importing routers
+// const recipesRouter = require("./routes/recipes/recipes.js")
+// const usersRouter = require("./routes/users/users.js")
+//middleware attaching
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express())
+// app.use("/recipes",recipesRouter)
+// app.use("/users",usersRouter)
+
+app.get("/",(req,res)=>{
+res.json("Live and Running")
+})
+const students ={
+subject: "math",
+teacher: "Mr. Marvin Bent",
+name: "Jill Scott",
+city: "NYC",
+grade: 0
+}
+const elementary = []
+
+app.listen(port,()=>{
+    console.log("youre runnin on port", port)
+})
+
+app.get("/students",(req,res)=>{
+
+res.json(students)
+
+})
+
