02556c0af81a30eac9fa954f8adda5f0334f5377
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-22 13:42:22 -0500

    attach and event listener to query button and console.log result in order to verify it is working

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+
+
 document.addEventListener("DOMContentLoaded",()=>{
 
+    const pullData = async ()=>{
+        try{
+            let res = await axios.get("http://localhost:3000/students")
+            console.log(res)
+        }catch(err){
+            console.log(err)
+                }
+        }
+
+let query = document.querySelector("#query")
+query.addEventListener("click",pullData()) 
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+// const wipeOut = ()=>{
+//     let choicebutton = document.querySelectorAll(".choicebutton")
+//     choicebutton.forEach((el)=>{
+//         el.addEventListener("click",()=>{
+//             let mainContainer = document.querySelector("#mainContainer")
+//             mainContainer.parentNode.removeChild(mainContainer)
+//             let secondMain = document.querySelector("#secondMain")
+//             secondMain.parentNode.appendChild(secondMain)    
+//                      })
+                   
+//          })
+       
+//      }
+//      wipeOut()
+// })
+
+
+
