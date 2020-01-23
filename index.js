7c61b3371ebe4783edd8319c4c483743dde00f9c
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-22 21:45:59 -0500

    backend working, frontend needs work

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const fetchData = async(url, callback) => {
-    let url = await axios.get('http://localhost:3000/')  
-    }
-    const fetchStudents = async(url, callback) => {
-        await axios.get('http://localhost:3000/students')  
-        }
-        console.log("this is a test")
-fetchClasses("/classes")
+    let class_ = document.querySelector("#class_")
+    let teacher_ = document.querySelector('#teacher')
+    let submitClass = document.querySelector('#submitClass')
+    
+    const add_class = (name, teacher) => {
+         axios.post('http://localhost:3000/class')
+        let res = res.data 
+        name: name
+        teacher: teacher
 
+        
+    }
+
+    submitClass.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        let ul = document.createElement('ul')
+        let li = document.createElement('li')
+        add_class(class_.value, teacher_.value)
+
+    })
 })
