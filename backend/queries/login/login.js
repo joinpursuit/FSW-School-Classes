11f229e25739902c27917584bda6226e8b83adcd
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-24 08:46:40 -0500

    add user existing checks

--- backend/queries/login/login.js
+++ backend/queries/login/login.js
@@ -1,5 +1,6 @@
 const {db} = require("../../db.js");
 
+// Checks if a user with the given username exists
 const isUsernameExisting = async (username) => {
     try {
         let user =  await db.any("SELECT * FROM logins WHERE username=$1", username);
@@ -13,6 +14,29 @@ const isUsernameExisting = async (username) => {
         console.log(err);
     }
 } // End of isUsernameExisting() function
+
+// Checks if the user matching that type and ID already has an account
+const isUserAccountExisting = async (userType, userId) => {
+    try {
+        console.log("Is account existing")
+        let user;
+        if(userType === "admins") {
+            user = await db.any('SELECT * FROM logins WHERE admin_id=$1', userId);
+        } else if(userType === "teachers") {
+            user = await db.any('SELECT * FROM logins WHERE teacher_id=$1', userId);
+        } else if(userType === "students") {
+            user = await db.any('SELECT * FROM logins WHERE student_id=$1', userId);
+        }
+
+        if(user.length) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch(err) {
+        console.log(err);
+    }
+} // End of isUserAccountExisting() function
 
 // Checks if the user exists by username & password
 const getUser = async (req, res) => {
@@ -40,14 +64,26 @@ const getUser = async (req, res) => {
 
 const createUser = async (req, res) => {
     try {
-        let {username, password, userType} = req.query;
+        let {username, password, userType, userId} = req.query;
         if(await isUsernameExisting(username)) {
             res.json({
                 error: "User with that name already exists.",
                 timestamp: new Date().toString()
             })
+        } else if(await isUserAccountExisting(userType, userId)) {
+            res.json({
+                error: "That person has an account already",
+                timestamp: new Date().toString()
+            })
         } else {
-            await db.none("INSERT INTO logins (username, passes, account_type) VALUES($1, $2, $3)", [username, password, userType]);
+            if(userType === "admins") {
+                await db.none("INSERT INTO logins (username, passes, admin_id) VALUES($1, $2, $3)", [username, password, userId]);
+            } else if(userType === "teachers") {
+                await db.none("INSERT INTO logins (username, passes, teacher_id) VALUES($1, $2, $3)", [username, password, userId]);
+            } else if(userType === "students") {
+                await db.none("INSERT INTO logins (username, passes, student_id) VALUES($1, $2, $3)", [username, password, userId]);
+            }
+
             res.json({
                 message: "User successfully created",
                 timestamp: new Date().toString()
@@ -73,11 +109,19 @@ const getPerson = async (req, res) => {
         }
        
         if(person.length) {
-            res.json({
-                person: person[0],
-                message: "User successfully found",
-                timestamp: new Date().toString()
-            })
+            if(await isUserAccountExisting(userType, person[0].id)) {
+                res.json({
+                    error: "That person has an account already", 
+                    timestamp: new Date().toString()
+                });
+            } else {
+                res.json({
+                    person: person[0],
+                    message: "User successfully found",
+                    timestamp: new Date().toString()
+                })
+            }
+            
         } else {
             res.json({
                 error: "No user found by that set of information",
