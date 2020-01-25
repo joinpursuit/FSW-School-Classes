0aee2f73b6a01aa80717597eff8c218949887928
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-25 11:03:05 -0500

    implement more frontend elements

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let class_ = document.querySelector("#class_")
-    let teacher_ = document.querySelector('#teacher')
+    let course = document.querySelector("#course")
+    let teacher = document.querySelector('#teacher')
     let submitClass = document.querySelector('#submitClass')
+    let addClass = document.querySelector('#addClass')
     
     const add_class = (name, teacher) => {
-         axios.post('http://localhost:3000/class')
-        let res = res.data 
-        name: name
-        teacher: teacher
-
+        axios.post('http://localhost:3000/class', {
+            //let res = res.data 
+            name: name,
+            teacher: teacher
+        })
         
+        
+        debugger;
     }
 
-    submitClass.addEventListener('submit', async (e) => {
+    addClass.addEventListener('submit', async (e) => {
         e.preventDefault()
         let ul = document.createElement('ul')
         let li = document.createElement('li')
-        add_class(class_.value, teacher_.value)
+        add_class(course.value , teacher.value)
+        debugger
+        req.body.appendchild(req.body)
 
     })
 })
