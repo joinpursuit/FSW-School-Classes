356cf3a07f9053029602ab69dfb8b76d76381c1f
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-25 16:58:41 -0500

    remove items off todo

--- todo.js
+++ todo.js
@@ -1,4 +1,1 @@
-//     Adds the ability to have multiple classes of the same name, with seperate teachers
 //     Maybe add a functionality to see/post teacher reviews
-
-// TODO: Add music to all pages
