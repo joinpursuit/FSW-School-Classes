fd10c7173ac66c56eba61d8d1ebadca284e0cc9f
ashya <ashyamanning@pursuit.org>
2020-01-26 01:59:38 -0500

    adding to studentForm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let classForm = document.querySelector("#addClass");
-    // let studentForm = document.querySelector("#addStudent");
-    // let listForm = document.querySelector("#studentList");
+    let studentForm = document.querySelector("#addStudent");
+    let listForm = document.querySelector("#studentList");
     let allClassesBtn = document.querySelector("#allClasses");
     allClassesBtn.addEventListener("click", async () => {
         let showAllClasses = document.querySelector("#showAllClasses");
@@ -18,10 +18,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
     classForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        // addClass();
-        addClassToDom()
+        addClassToDom();
     })
 
+    listForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        checkBoxChange();
+
+    })
+
+    studentForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        let className = document.querySelector("#class").value;
+        let studentName = document.querySelector("#studentName").value;
+        let age = document.querySelector("#age").value;
+        let city = document.querySelector("#city").value;
+        let grade = document.querySelector("#grade").value;
+        let container = document.querySelector("#addStudentDiv");
+        let p = document.createElement("p");
+        let 
+        try {
+            let res = await axios.post(`http://localhost:3000/class/${className}/enroll`, {
+                className,
+                studentName,
+                age,
+                city,
+                grade
+            })
+            return res;
+        } catch (err) {
+            console.log(err)
+        }
+    })
 })
 
 const addClass = async () => {
@@ -32,28 +60,56 @@ const addClass = async () => {
             name,
             teacher
         });  
-        console.log(res)
-        return res 
+        // console.log(res)
+        return res; 
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
 const addClassToDom = async () => {
-    data = await addClass()
+    data = await addClass();
     console.log(data.data['class']);
     
-    let container = document.querySelector("#addClass");
+    let container = document.querySelector("#addClassDiv");
 
     let newClass = document.createElement("div");
     newClass.id = 'newClass';
     let className = document.createElement("p");
     let teacherName = document.createElement("p");
     
-    className.innerText = data.data.class.name
-    teacherName.innerText = data.data.class.teacher
+    className.innerText = data.data.class.name;
+    teacherName.innerText = data.data.class.teacher;
 
-    newClass.append(className,teacherName)
+    newClass.append(className,teacherName);
 
-    container.append(newClass)
+    container.append(newClass);
+}
+
+const checkBoxChange = () => {
+    let check = document.querySelector("#checkbox");
+    let classList = document.querySelector("#classList").value;
+    let cityList = document.querySelector("#cityList").value;
+    check.addEventListener("change", async () => {
+        // console.log(check.checked);
+        try {
+            let res = await axios.get("http://localhost:3000/class");
+            let result = [];
+            let allClasses = res.data.allClasses.classList; 
+            console.log(allClasses);
+            allClasses.forEach(el => {
+                if (el.toLowerCase() === classList.toLowerCase()) {
+                    el['students'].forEach(student => {
+                        if (student.city === cityList && student.grade < 70) {
+                            result.push(student);
+                        }
+                        return result;
+                    })
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    })
+    return check.checked;
 }
