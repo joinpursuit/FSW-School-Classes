8c8e9d42fda24819eda34faa2154e50010d45c29
dannyl <danilovlaurent@pursuit.org>
2020-01-26 13:58:23 -0500

    create css for frontend

--- School.js
+++ School.js
@@ -1,96 +1,80 @@
-const Class = require('./Class');
-const Student = require('./Student')
+const Class = require("./Class");
+const Student = require("./Student");
 
 class School {
   constructor() {
     this.classes = {
-          physics: {
-          name: "physics",
-          teacher: "Jon A",
-          students: [
-            {name: "slater",
+      physics: {
+        name: "physics",
+        teacher: "Jon A",
+        students: [
+          { name: "slater", age: 14, city: "Brooklyn", grade: 35 },
+          {
+            name: "zach",
             age: 14,
             city: "Brooklyn",
-            grade: 35
-            },
-            {
-              "name": "zach",
-              "age": 14,
-              "city": "Brooklyn",
-              "grade": 98
+            grade: 98
           },
           {
-            "name": "screech",
-            "age": 14,
-            "city": "Queens",
-            "grade": 45
+            name: "screech",
+            age: 14,
+            city: "Queens",
+            grade: 45
           },
           {
-            "name": "lisa",
-            "age": 14,
-            "city": "Queens",
-            "grade": 98
+            name: "lisa",
+            age: 14,
+            city: "Queens",
+            grade: 98
           },
           {
-            "name": "jessy",
-            "age": 14,
-            "city": "Queens",
-            "grade": 69
+            name: "jessy",
+            age: 14,
+            city: "Queens",
+            grade: 69
           }
-          ]
-        } 
-    }
+        ]
+      }
+    };
   }
-  addClassToClasses(name,teacher){
-    this.addClass(name,teacher);
-    return this.classes[name]
+  addClassToClasses(name, teacher) {
+    this.addClass(name, teacher);
+    return this.classes[name];
   }
   addClass(name, teacher) {
     let newClass = new Class(name, teacher);
     this.classes[name] = newClass;
-    return this.classes[name]
+    return this.classes[name];
   }
 
-  enrollStudent(className,student,age,city) {
+  enrollStudent(className, student, age, city) {
     let studentsarr = this.classes[className].students;
-    let newStudent = new Student(student,age,city,"N/A");
+    let newStudent = new Student(student, age, city, "N/A");
     studentsarr.push(newStudent);
     return newStudent;
   }
 
   getStudentsByClass(className) {
-    return this.classes[className].students
+    return this.classes[className].students;
   }
 
   getStudentsByClassWithFilter(className, failing, city) {
-    let studentsArr = this.classes[className].students
-    
-    
+    let studentsArr = this.classes[className].students;
+
     return studentsArr.filter(student => {
-      if(failing  &&  student.grade < 70 && student.city === city){
-        return student
-
-      } else if (failing === undefined && student.city === city){
-        return student
-
-      } else if (failing && student.grade < 70 && city === undefined){
-        return student
+      if (failing && student.grade < 70 && student.city === city) {
+        return student;
+      } else if (failing === undefined && student.city === city) {
+        return student;
+      } else if (failing && student.grade < 70 && city === undefined) {
+        return student;
       }
-
-
-    })
-    
+    });
   }
-
 }
-
-
 
 let mySchool = new School();
 
 // console.log(mySchool.getStudentsByClassWithFilter("physics",false,"Queens"));
-
-
-
 
 module.exports = mySchool;
--- routes/classRouter.js
+++ routes/classRouter.js
@@ -1,107 +1,100 @@
 const classRouter = require("express").Router();
-const mySchool = require("./../School.js")
+const mySchool = require("./../School.js");
 const displayTime = require("./../build.js");
 
-
-
-classRouter.post("/",(req,res)=> {
-    res.json(req.body);
+classRouter.post("/", (req, res) => {
+  res.json(req.body);
 });
 
+classRouter.post("/add", (req, res) => {
+  let name = req.body.name;
+  let teacher = req.body.teacher;
 
-classRouter.post("/add",(req,res)=> {
-    let name    = req.body.name;
-    let teacher = req.body.teacher;
-    
-    if(mySchool.classes[name]){
-        res.json({
-            "error": "Please fill out all the information or Class already exists",
-            "timestamp": displayTime()
-        })
+  if (mySchool.classes[name]) {
+    res.json({
+      error: "Please fill out all the information or Class already exists",
+      timestamp: displayTime()
+    });
+  } else {
+    mySchool.addClass(name, teacher);
+
+    res.json({
+      class: mySchool.classes[name],
+      message: "Created a new class",
+      status: "ok",
+      timestamp: displayTime()
+    });
+  }
+});
+
+classRouter.post("/:className/enroll", (req, res) => {
+  let className = req.params.className;
+  let studentName = req.body.name;
+  let studentAge = req.body.age;
+  let studentCity = req.body.city;
+  let studentGrade = req.body.grade;
+  let studentsArr = mySchool.classes[className].students;
+
+  studentsArr.forEach(student => {
+    if (studentName === "" || studentAge === "" || studentCity === "") {
+      res.json({
+        error: "Please fill out all the information for the student",
+        timestamp: displayTime()
+      });
+    } else if (student.name === studentName) {
+      student.name = studentName;
+      student.age = studentAge;
+      student.city = studentCity;
+      student.grade = studentGrade;
     } else {
-        mySchool.addClass(name,teacher);
-
-        res.json({
-            "class": mySchool.classes[name],
-            "message": "Created a new class",
-            "status":"ok",
-            "timestamp": displayTime()
-        })
+      let result = mySchool.enrollStudent(
+        className,
+        studentName,
+        studentAge,
+        studentCity
+      );
+      res.json({
+        student: result,
+        className: className,
+        message: "Enrolled Student",
+        timestamp: displayTime()
+      });
     }
+  });
 });
 
+classRouter.get("/:className/students", (req, res) => {
+  let classChosen = req.params.className;
+  let failing = req.query.failing;
+  let city = req.query.city;
+  let obj = req.query;
 
-classRouter.post("/:className/enroll",(req,res)=> {
-    let className = req.params.className;
-    let studentName = req.body.name;
-    let studentAge = req.body.age;
-    let studentCity = req.body.city;
-    let studentGrade = req.body.grade;
-    let studentsArr = mySchool.classes[className].students
-    
-    studentsArr.forEach(student => {
-        if(studentName === "" || studentAge === "" || studentCity === ""){
-            res.json({
-                "error": "Please fill out all the information for the student",
-                "timestamp": displayTime()
-            })
-        }else if(student.name === studentName){
-            student.name = studentName;
-            student.age = studentAge;
-            student.city = studentCity;
-            student.grade = studentGrade;
-        } else{
-            let result = mySchool.enrollStudent(className,studentName,studentAge,studentCity);
-            res.json({
-                "student": result,
-                "className": className,
-                "message": "Enrolled Student",
-                "timestamp": displayTime()
-            })
-        } 
-    })
+  if (Object.keys(obj).length < 1) {
+    let students = mySchool.getStudentsByClass(classChosen);
+    console.log(mySchool.classes[classChosen]);
+
+    res.json({
+      students: students,
+      message: "Retrieved Students",
+      timestamp: displayTime()
+    });
+  } else if (Object.keys(obj).length >= 1) {
+    let students2 = mySchool.getStudentsByClassWithFilter(
+      classChosen,
+      failing,
+      city
+    );
+    res.json({
+      students: students2,
+      message: "Retrieved Students",
+      timestamp: displayTime()
+    });
+  } //else if(mySchool.classes[classChosen].name === false) {
+  // res.json({
+  //     "error": `Class ${classChosen} doesn't exist.`,
+  //     "timestamp": timestamp()
+  //   })
+  // }
 });
-
-
-
-classRouter.get("/:className/students",(req,res)=> {
-    
-    let classChosen =  req.params.className
-    let failing = req.query.failing
-    let city = req.query.city
-    let obj = req.query
-    
-    
-    if(Object.keys(obj).length < 1){
-       let students =  mySchool.getStudentsByClass(classChosen)
-       console.log(mySchool.classes[classChosen]);
-       
-        res.json({
-            "students": students,
-            "message": "Retrieved Students",
-            "timestamp": displayTime()
-          })
-        
-    } else if (Object.keys(obj).length >= 1)  {
-        
-        let students2 = mySchool.getStudentsByClassWithFilter(classChosen,failing,city)
-        res.json({
-            "students": students2,
-            "message": "Retrieved Students",
-            "timestamp": displayTime()
-          })
-
-    } //else if(mySchool.classes[classChosen].name === false) { 
-        // res.json({ 
-        //     "error": `Class ${classChosen} doesn't exist.`,
-        //     "timestamp": timestamp()
-        //   })
-    // }
-});
-
-
-
-
-
 
 module.exports = classRouter;
