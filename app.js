5b3c09afec4996452b590283fc9b29f4786a9829
maria liza <mariamartinez@pursuit.org>
2020-01-27 00:25:32 -0500

    remove more dead code

--- app.js
+++ app.js
@@ -12,37 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/class", classesRouter)
 
-// app.get("/hello", function(req, res) {
-//   res.send("Hello World!")
-// })
-
-// app.get("/", function(req, res) {
-//   res.send("index page")
-// })
-
-// app.post("/class/:classname/students", function(req, res) {
-// //   let skl = new School()
-// //   skl.addClass("physics1", "mr.tang")
-// //   skl.addClass("physics2", "ms.maria")
-
-// //     let st = new Student("hernandez", "8", "bronx", "12")
-// //     let st2 = new Student("jon", "10", "queens", "11" )
-// // // console.log("we loggin")
-// //     skl.enrollStudent("physics1", st)
-// //     skl.enrollStudent("physics1", st2)
-// //     skl.enrollStudent("physics2", st2)
-// //   res.json(skl.getStudentsByClass(req.params.classname))
-
-//   res.json("this works")
-
-// })
-
-// // addClass(name, teacher) {
-// //   let newClass = new Class(name, teacher);
-// //   this.classes[name] = newClass;
-
-// app.post("/class/:name/:teacher", )
-
 app.listen(port, () => {
   console.log("Listening on port " + port)
 })
