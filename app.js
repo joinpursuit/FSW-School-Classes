401e2a2068952e3695d5164c6d421b078de1df36
crystal cardona <crystalcardona@pursuit.org>
2020-01-27 15:27:52 -0500

    working on error handles

--- app.js
+++ app.js
@@ -1,90 +1,80 @@
 const express = require("express");
 const cors = require("cors");
-const app = express(); 
+const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const School = require("./School.js");
-const Student = require("./Student.js")
-
+const Student = require("./Student.js");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let mySchool = new School()
-const date = new Date ()
+let mySchool = new School();
+const date = new Date();
 
 app.post("/class/:className/enroll", (req, res) => {
-    
-    let newStudent = new Student(req.body.name, req.body.age, req.body.city, req.body.grade)
-    
-   let studentInfo = mySchool.enrollStudent(req.params.className, newStudent)
-        res.json({
-            "student": studentInfo,
-            "className": req.params.className,
-            "message": "Enrolled Student",
-            "timestamp": date
-            })
-    })
+  try {
+    let newStudent = new Student(
+      req.body.name,
+      req.body.age,
+      req.body.city,
+      req.body.grade
+    );
 
-
+    let studentInfo = mySchool.enrollStudent(req.params.className, newStudent);
+    res.json({
+      student: studentInfo,
+      className: req.params.className,
+      message: "Enrolled Student",
+      timestamp: date
+    });
+  } catch (err) {
+    res.json({
+      error: "Please fill out all the information for the student",
+      timestamp: date
+    });
+  }
+});
 
 app.get("/:className/students", (req, res) => {
-    let className = req.params.className
-    let failing = req.query.failing 
-    let city = req.query.city
-    let listOfStudents = mySchool.getStudentsByClass(className);
-    if(!listOfStudents){
-        res.json({
-            "error": `Class ${className} doesn't exist.`,
-            "timestamp": date
-        })
-    } else {
-        res.json({
-            name: className,                                                                                                                                                                                                                                                    
-            students: listOfStudents,
-        })
-    }
-
-})
-
-
+  let className = req.params.className;
+  let failing = req.query.failing;
+  let city = req.query.city;
+  let listOfStudents = mySchool.getStudentsByClass(className);
+  if (!listOfStudents) {
+    res.json({
+      error: `Class ${className} doesn't exist.`,
+      timestamp: date
+    });
+  } else {
+    res.json({
+      name: className,
+      students: listOfStudents
+    });
+  }
+});
 
 app.post("/class/:name/:teacher", (req, res) => {
-        let name = req.params.name
-        let teacher = req.params.teacher
-    if(mySchool.classes.className === name ){
-        res.json({
-            error: "Please fill out all the information or Class already exists",
-            timestamp: date
-        })
-    } else {
-         mySchool.addClass(name, teacher)
-         res.json({
-             class: { name: name, teacher: teacher, 
-             students: []},
-             message: "Created new class",
-             timestamp: date
+  let name = req.params.name;
+  let teacher = req.params.teacher;
+  if (!mySchool.classes.className) {
+    mySchool.addClass(name, teacher);
+    res.json({
+      class: { name: name, teacher: teacher, students: [] },
+      message: "Created new class",
+      timestamp: date
+    });
+  } else {
+    res.json({
+      error: "Please fill out all the information or Class already exists",
+      timestamp: date
+    });
+  }
+});
 
-
-         })
-           
-    }
-        
-
-})
-
-app.get("/class", (req,res) => {
-    res.json(mySchool)
-})
-
-
-
-
-
-
-
-
-
+app.get("/class", (req, res) => {
+  res.json(mySchool);
+});
 
 app.listen(port, () => "listening");
--- index.js
+++ index.js
@@ -1,80 +1,80 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let className = document.querySelector("#className");
+  let teacherName = document.querySelector("#teacherName");
+  let addClass = document.querySelector("#addClass");
+  let enrollClass = document.querySelector("#studentClass");
+  let enrollName = document.querySelector("#studentName");
+  let enrollAge = document.querySelector("#studentAge");
+  let enrollCity = document.querySelector("#studentCity");
+  let enrollGrade = document.querySelector("#studentGrade");
+  let classList = document.querySelector("#classList");
+  let cityList = document.querySelector("#cityList");
+  let checkFailing = document.querySelector("#checkFailing");
+  let listSubmit = document.querySelector("#listSubmit");
+  let form1 = document.querySelector("#one");
+  let form2 = document.querySelector("#two");
+  let form3 = document.querySelector("#three");
+  let div1 = document.querySelector("#divOne");
+  let div2 = document.querySelector("#divTwo");
+  let div3 = document.querySelector("#three");
 
-    let className = document.querySelector("#className");
-    let teacherName = document.querySelector("#teacherName");
-    let addClass = document.querySelector("#addClass");
-    let enrollClass = document.querySelector("#studentClass");
-    let enrollName = document.querySelector("#studentName");
-    let enrollAge = document.querySelector("#studentAge");
-    let enrollCity = document.querySelector("#studentCity");
-    let enrollGrade = document.querySelector("#studentGrade");
-    let classList = document.querySelector("#classList");
-    let cityList = document.querySelector("#cityList");
-    let checkFailing = document.querySelector("#checkFailing");
-    let listSubmit = document.querySelector("#listSubmit")
-    let form1 = document.querySelector("#one")
-    let form2 = document.querySelector("#two")
-    let form3 = document.querySelector("#three")
-    let div1 = document.querySelector("#divOne")
-    let div2 = document.querySelector("#divTwo")
-    let div3 = document.querySelector("#three")
+  form1.addEventListener("submit", async e => {
+    e.preventDefault();
+    try {
+      axios.post(`http://localhost:3000/class/${className.value}/${teacherName.value}`).then(res => {
+          let result = document.createElement("p");
+          let newClass =
+            className.value.charAt(0).toUpperCase() + className.value.slice(1);
+          let teacher =
+            teacherName.value.charAt(0).toUpperCase() +
+            teacherName.value.slice(1);
+          result.innerText = `Teacher: ${teacher} : Class: ${newClass}`;
+          div1.appendChild(result);
+          debugger;
+          alert(res.data.message);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
- 
+  form2.addEventListener("submit", async e => {
+    e.preventDefault();
+    try {
+      axios.post(`http://localhost:3000/class/${enrollClass.value}/enroll`, {
+          name: enrollName.value,
+          age: enrollAge.value,
+          city: enrollCity.value,
+          grade: enrollGrade.value
+        }).then(res => {
+          let enrolledStudent = res.config.data;
+          let result = document.createElement("p");
+          result.innerText = `New Enrolled Student: ${enrolledStudent}`;
+          div2.appendChild(result);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-    form1.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        try { 
-        axios.post(`http://localhost:3000/class/${className.value}/${teacherName.value}`).then(res => {
-            let result = document.createElement("p");
-            let newClass = className.value.charAt(0).toUpperCase() + className.value.slice(1)
-            let teacher = teacherName.value.charAt(0).toUpperCase() + teacherName.value.slice(1)
-            result.innerText = `Teacher: ${teacher} : Class: ${newClass}` 
-            div1.appendChild(result)
-            debugger
-            }) 
-        }catch(err){
-            console.log(err)
-        }
-        
-    }) 
-
-    form2.addEventListener("submit", async (e) => {
-        e.preventDefault()
-        try {
-            axios.post(`http://localhost:3000/class/${enrollClass.value}/enroll`, {name: enrollName.value, age: enrollAge.value, city: enrollCity.value, grade: enrollGrade.value}).then(res => {
-                let enrolledStudent = res.config.data
-                    let result = document.createElement("p")
-                    result.innerText = `New Enrolled Student: ${enrolledStudent}`
-                    div2.appendChild(result)
-                })
-        } catch (err){
-            console.log(err)
-        }
-    })
-
-
-    form3.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        try{
-            axios.get(`http://localhost:3000/${classList.value}/students`).then(res => {
-                let name = res.data.name
-                let students = res.data.students
-                if(students){
-                    students.forEach(student => {
-                        let p = document.createElement("p");
-                        p.innerText = `Class: ${name.charAt(0).toUpperCase() + name.slice(1)}, 
-                                        Student List: ${student.name.charAt(0).toUpperCase() + student.name.slice(1)}`
-                        div3.appendChild(p)
-                    })
-                }
-                
-            })
-        }catch(err){
-            console.log(err)
-        }
-    })
-
-
-    
-
-})
+  form3.addEventListener("submit", async e => {
+    e.preventDefault();
+    try {
+      axios.get(`http://localhost:3000/${classList.value}/students`).then(res => {
+          let name = res.data.name;
+          let students = res.data.students;
+          let p = document.createElement("p");
+          if (students) {
+            students.forEach(student => {
+              let p = document.createElement("p");
+              p.innerText = `Class: ${name.charAt(0).toUpperCase() +name.slice(1)}, 
+                            Student List: ${student.name.charAt(0).toUpperCase() +student.name.slice(1)}`;
+              div3.appendChild(p);
+            });
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
