d9ed86e26e446f3b305c3d5e4ad684371da9180c
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-01-26 19:06:40 -0500

    complete form one

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const School = require('./School.js');
+const Student = require('./Student.js');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const port = 3000;
+const port = 4000;
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // const classRouter = require("./Class.js");
@@ -20,52 +21,65 @@ app.use(express.urlencoded({extended: false}));
 
 let mySchool = new School();
 
+app.get("/", (req, res) => {
+    try{
+        res.json(mySchool);
+    } catch(error) {
+        console.log(error);
+    }
+});
 
-app.get("/class", (req, res) => {
+app.post("/class", (req, res) => {
     try {
-
-        debugger;
+        mySchool.addClass(req.body.name, req.body.teacher);
+        res.json({ 
+            "class": { "name": req.body.name, "teacher": req.body.teacher, "students": []},
+            "message": "Created a new class",
+            "timestamp": "YYYY, MM/DD HH:MM:SS"
+          })
     } catch(error) {
         res.json({ 
             "error": "Please fill out all the information or Class already exists",
             "timestamp": "YYYY, MM/DD HH:MM:SS"
-        });
+          });
     };
-    // console.log(req)
 });
 
 app.post("/:name/enroll", (req, res) => {
     try {
-
+        mySchool.enrollStudent(req.body.class, req.body.name);
+        res.json({
+                "student": { "name": req.body.name, "age": req.body.age, "city": req.body.city, "grade": req.body.grade },
+                "className": req.body.class,
+                "message": "Enrolled Student",
+                "timestamp": "YYYY, MM/DD HH:MM:SS"
+        });
     } catch(error) {
-        
-    };
+        res.json({
+            "error": "Please fill out all the information for the student",
+            "timestamp": "YYYY, MM/DD HH:MM:SS"
+        })
+    }
 });
 
 app.get("/:className/students", (req, res) => {
     try {
-
+        mySchool.getStudentsByClassWithFilter(req.body.name, req.body.failing, req.body.city);
+        res.json({
+            "students": [
+            { "name": req.body.name, "age": req.body.age, "city": req.body.city, "grade": req.body.grade },
+            { "name": "Emily", "age": 28, "city": "LA", "grade": 80 }
+            ],
+            "message": "Retrieved Students",
+            "timestamp": "YYYY, MM/DD HH:MM:SS"
+        });
     } catch(error) {
-
+        res.json({
+            "error": `Class ${req.body.class} doesn't exist.`,
+            "timestamp": "YYYY, MM/DD HH:MM:SS"
+        });
     };
 });
-
-// app.post("/users", (req, res) => {
-//     res.json("created a new user");
-// })
-
-// app.get("/posts", (req, res) => {
-//     res.json("returns all posts");
-// })
-
-// app.get("/posts/:id", (req, res) => {
-//     res.json("returns a specific post")
-// })
-
-// app.get("/users/:id/posts", (req, res) => {
-//     res.json("all the posts for a specific user");
-// })
-
 
 app.listen(port, () => {
     console.log("Listening on port: ", port);
--- frontend/homepage.js
+++ frontend/homepage.js
@@ -1,9 +1,34 @@
-const addClassForm = document.querySelector(".form1");
-addClassForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    let className = document.querySelector("#class1");
-    let teacherName = document.querySelector("#teacherName");
-    await axios.post("http://localhost:3000/class", {name: className.value, teacher: teacherName.value, students: []});
-    console.log(res.data);
-    debugger;
+document.addEventListener("DOMContentLoaded", () => {
+    const addClassForm = document.querySelector("#form1");
+    addClassForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        let addClassName = document.querySelector("#class1");
+        let addTeacherName = document.querySelector("#teacherName");
+        let res = await axios.post("http://localhost:4000/class", {name: addClassName.value, teacher: addTeacherName.value});
+        console.log(res.data);
+        let ul = document.querySelector("ul");
+        let li = document.createElement("li");
+        li.innerText = res.data.message;
+        ul.appendChild(li);
+    });
+    
+    const enrollStudentForm = document.querySelector("#form2");
+    enrollStudentForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        let enrollClassName = document.querySelector("#class2");
+        let enrollStudentName = document.querySelector("#studentName");
+        let enrollAge = document.querySelector("#age");
+        let enrollCity = document.querySelector("#city2");
+        let enrollGrade = document.querySelector("#grade");
+        let res = await axios.post("http://localhost:4000/class/%{enrollClassName.value}/enroll", {class: enrollClassName.value, name: enrollStudentName.value, age: enrollAge.value, city: enrollCity.value, grade: enrollGrade.value});
+    });
+    
+    // const listStudentForm = document.querySelector("#form3");
+    // listStudentForm.addEventListener("submit", async (event) => {
+    //         event.preventDefault();
+    //         let listStudentClass = document.querySelector("#class3");
+    //         let listStudentCity = document.querySelector("#city3");
+        
+    //         let res = await axios.get("http://localhost:4000/class/%{listStudentClass.value}/students", {})
+    //     })
 })
