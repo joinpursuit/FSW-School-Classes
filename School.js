7295cf3c6566fc6f05e6d55417a846da5b08f7f8
Kelvin <kelvinarellano@pursuit.org>
2020-01-26 23:59:39 -0500

    initial files created

--- School.js
+++ School.js
@@ -4,66 +4,31 @@ const Student = require('./Student')
 class School {
   constructor() {
     this.classes = {
-      // className: Class Object
-      //   physics: {} 
     }
   }
 
-  /**
-   * Add class to classes
-   * 
-   * @param {string} name - Name of the class
-   * @param {string} teacher - Name of instructor 
-   * @return {Class} Class object
-   */
   addClass(name, teacher) {
     let newClass = new Class(name, teacher);
     this.classes[name] = newClass;
   }
 
-  /**
-   * Enroll student in class
-   * 
-   * @param {string} className - Name of the class
-   * @param {Student} student - Student object
-   * @return {Student} Enrolled student
-   */
+
   enrollStudent(className, student) {
-    // Your code here
+    let newStudent = new Student(student.name, student.age, student.city, student.grade)
+    this.class[className]["student"].push(newStudent)
   }
 
 
-
-
-  /**
-   * Get all students enrolled in a class
-   * 
-   * @param {string} className - Name of the class
-   * @return {Student[]} Array of Student objects
-   */
   getStudentsByClass(className) {
-    // Your code here
+    return this.classes[className].students
   }
 
 
 
 
-  /**
-   * Get all students and apply filters. If failing = true
-   * return all students that are failing the class, 
-   * that is all students whose grade is less than 70.
-   * If a city is passed return students whose city match
-   * the city passed. If both failing and city are passed
-   * return students that are failing and that live in the
-   * specified city
-   * 
-   * @param {string} className - Name of the class
-   * @param {boolean} failing - Whether to return students that are failing the class or not
-   * @param {string} city - Name of the city to match against students
-   * @return {Student[]} Array of Student objects
-   */
+  
   getStudentsByClassWithFilter(className, failing, city) {
-    // Your code here
+    let students = this
   }
 
 }
