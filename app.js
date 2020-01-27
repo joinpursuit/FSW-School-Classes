962a89948045c17c38d9441b417c6132f26f6091
Jay Fowler <jcafowler@gmail.com>
2020-01-26 20:10:31 -0500

    Fix Enroll Student Form

--- app.js
+++ app.js
@@ -1,48 +1,67 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const timestamp = require("express-timestamp")
-const School = require("./School.js")
-const port = 3000
-const app = express()
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(timestamp.init)
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const timestamp = require("express-timestamp");
+const School = require("./School.js");
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(timestamp.init);
 
-let mySchool = new School()
+let mySchool = new School();
 
 app.get("/classes", (req, res) => {
-  try{
+  try {
     res.json({
       classes: mySchool.classes,
       message: "Request has been completed successfully",
       timestamp: req.timestamp
-    })
-  } catch (err){
+    });
+  } catch (err) {
     res.json({
       error: "Your request could not be completed",
       timestamp: req.timestamp
-    })
+    });
   }
-})
+});
 
 app.post("/classes/add", (req, res) => {
+  try {
+    mySchool.addClass(req.body.class, req.body.teacher);
+    res.json({
+      message: `${req.body.class} was added successfully.`,
+      timestamp: req.timestamp
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      error:
+        "Class could not be created. Class may already exist. Please try again.",
+      timestamp: req.timestamp
+    });
+  }
+});
+app.post("/classes/enroll", (req, res) => {
+  
   try{
-    mySchool.addClass(req.body.class, req.body.teacher)
-  res.json({
-    message: `${req.body.class} was added successfully.`,
-    timestamp: req.timestamp
-  })
+    let stuInfo = req.body
+    mySchool.enrollStudent(stuInfo.class, {name: stuInfo.name, age: stuInfo.age, grade: stuInfo.grade, city: stuInfo.city})
+    res.json({
+      info: mySchool.classes[stuInfo.class].students,
+      message: `${stuInfo.name} has been successfully enrolled in ${stuInfo.class}.`,
+      timestamp: timestamp.init
+    })
   } catch(err){
     console.log(err)
     res.json({
-      error: "Class could not be created. Class may already exist, please try again.",
+      error: "Student could not be enrolled. Student may already be in class. Please try again.",
       timestamp: req.timestamp
     })
   }
-  
-})
+});
+
 app.listen(port, () => {
-  console.log(`The server is running on port ${port}`)
-})
+  console.log(`The server is running on port ${port}`);
+});
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   let newClassDiv = document.querySelector("#newClassDiv")
   let newClassForm = document.querySelector("#newClassForm")
-  let classList = document.querySelector("#classList")
-
-  const populateClasses = async () => {
-    
-  }
+  let newStudentDiv = document.querySelector("#addNewStudent")
+  let newStudentForm = document.querySelector("#newStudentForm")
 
   newClassForm.addEventListener("submit", async (e) => {
     e.preventDefault()
@@ -17,6 +14,22 @@ document.addEventListener("DOMContentLoaded", () => {
     p.innerText = res.data.message
     newClassDiv.appendChild(p)
     newClassForm.reset()  
+  })
+
+  newStudentForm.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    let className = document.querySelector("#class").value
+    let name = document.querySelector("#name").value
+    let age = document.querySelector("#age").value
+    let grade = document.querySelector("#grade").value
+    let city = document.querySelector("#city").value
+
+    let res = await axios.post("http://localhost:3000/classes/enroll",{class: className, name, age, grade, city})
+    let p = document.createElement("p")
+    console.log(res.data)
+    p.innerText = res.data.message
+    newStudentDiv.appendChild(p)
+
   })
 
 })
