782c6f283a75351be160f13abc685f281e0b1e17
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-31 14:10:10 -0500

    get class list route and failing route completed

--- routes/classes.js
+++ routes/classes.js
@@ -2,11 +2,9 @@ const express = require("express")
 const cors = require("cors")
 const School = require("../School")
 const Student = require("../Student")
-
 let classes = express.Router()
 classes.use(cors())
 let mySchool = new School();
-
 classes.get("/", (request, response) => {
     response.json(mySchool)
 })
@@ -14,14 +12,12 @@ classes.get("/", (request, response) => {
 classes.post("/", (request, response) => {
     let classes = request.body.classes;
     let teacher = request.body.teacher;
-    // console.log(classes)
-    // console.log(teacher)
     if(classes.length===0|| teacher.length===0){
-        response.json({message: "type in valid entries"})
+        response.json({message: "Please fill out all the information or Class already exists"})
     }else{
-    mySchool.addClass(classes, teacher)
+    mySchool.addClass(classes, teacher,)
     response.json({
-        "classes": { "nameOfClass": classes, "teacher": teacher },
+        "classes": { "nameOfClass": classes, "teacher": teacher ,"students": []},
         "message": `Created a ${classes} class taught by ${teacher}`,
         "timestamp": new Date()
     })
@@ -34,29 +30,34 @@ classes.post("/enroll", (request, response) => {
     let age = request.body.age;
     let city = request.body.city;
     let grade = request.body.grade;
-    // let studentInfo = new Student(nameOfStudentInput,ageInput, cityInput, gradeInput).value
     if(student.length=== 0|| age.length === 0|| city.length=== 0|| grade.length === 0){
-        response.json({message: "type in valid entries"})
+        response.json({message: "Please fill out all the information for the student"})
     }else{
-    //     mySchool.enrollStudent(studentInfo)
     mySchool.enrollStudent(classes, student, age, city, grade)
     response.json({
-        "student": { 'student': student },
+        "student": { 'student': new Student() },
         "class": { 'classes': classes },
-        "message": `you ${student} have been enrolled in ${classes} Thank You!`,
+        "message": ` ${student}, you have been enrolled in ${classes} Thank You!`,
         "timestamp": new Date()
     })
     }
 })
 
 classes.get("/:className/lists", (request, response) => {
-    let className = request.body.className;
-    mySchool. getStudentsByClass(className)
+    let className = request.params.className;
+    mySchool.getStudentsByClass(className)
     response.json({
-        "className": { "classes": className },
+        "classes": { "classes": classes },
         "Time": new Date(),
-        "message": `${className}, list of students`
+        "message": `${classes}, list of students`
     })
+
 })
+classes.get("/:className/lists/:failing", (request, response)=>{
+    let className = request.params.className;
+    let failing = request.params.failing;
+    response.json(mySchool.getStudentsByClassWithFilter(className, failing))
+})
+
 
 module.exports = classes;
