19ba2f9f4416ed34047327f1764db03b8798a891
Henrynunez112 <henrynunez@pursuit.org>
2020-02-02 20:17:50 -0500

    redid all the routes

--- app.js
+++ app.js
@@ -12,32 +12,68 @@ const Student = require("./Student.js");
 
 let mySchool = new School();
 
-app.get("/", (req, res)=>{
-   let {name, teacher} = req.body;
-
-    if(!mySchool.classes.hasOwnProperty(name) && name !== "" && teacher !== ""){
-        mySchool.addClass(name, teacher);
-        res.json({
-            class: { name: name, teacher: teacher, students:[]},
-            message: "Created a new class",
+app.post("/class", (req, res)=>{
+    try{
+        let subject = new Class(
+            req.body.name,
+            req.body.teacher
+        )
+        let newSubject = mySchool.addClass(subject);
+        res.status(200).json({
+            class: newSubject,
+            message: "You have created a new class",
             timestamp: new Date().toString()
-          });
-    }else{
-        res.json({
-            error: "Either some information is missing or the class already exist",
+        })
+    }catch(err){
+        res.status(200).json({
+            error: "Please fill out all the information required",
             timestamp: new Date().toString()
-              
-        });
+        })
     }
 })
 
-app.post("/class", (req, res)=>{
+app.post("/class/:className/enroll", (req, res)=>{
+    try{
+        let newStudent = new Student (
+            req.body.name,
+            req.body.age,
+            req.body.city,
+            req.body.grade
+        )
 
-        let newClass = mySchool.addClass(req.body.name, req.body.teacher);
-        res.json(newClass);
-        console.log(newClass)
+        let studentInfo = mySchool.enrollStudent(req.params.className, newStudent)
+        res.status(200).json({
+            student: studentInfo,
+            message: "these are the enrolled students",
+            timestamp: new Date().toString()
+        })
+
+    }catch(err){
+        res.status(200).json({
+            error: "this class does not exist",
+            timestamp: new Date().toString()
+        })
+    }
 
 });
+
+app.get("/class/:className/students", (req, res)=>{
+    try{
+        let allStudents = mySchool.getStudentsByClass(req.params.className);
+        res.status(200).json({
+            students: allStudents,
+            message: "Retrived Students",
+            timestamp: new Date().toString()
+        })
+    }catch(err){
+
+        res.status(200).json({
+            message: "this class does not exist",
+            timestamp: new Date().toString()
+        })
+
+    }
+})
 
 
 
