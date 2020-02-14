0eddc5d534cae43ad94ae9a565a94791dd36ff0d
Kevin Brutus <kevinbrutus@pursuit.org>
2020-02-14 13:19:15 -0500

    move id from html buttons to the forms. add event to functions to prevent page refresh. add event listeners to the forms

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-const addClass = async () => {
+let classFrm = document.querySelector("#addClass")
+let enrollFrm = document.querySelector("#addStudent")
+let searchFrm = document.querySelector("#listStudents")
+
+const addClass = async (event) => {
+    event.preventDefault();
     let className = document.querySelector("#className").value;
     let teacher = document.querySelector("#teacher").value;
     axios.post("http://localhost:3000/class", {className, teacher});
-    className.innerHTML = " "
-    teacher.innerHTML = " "
+    className.innerHTML = " ";
+    teacher.innerHTML = " ";
 }
 
-const enrollStudent = async () => {
+const enrollStudent = async (event) => {
+    event.preventDefault();
     let addClass = document.querySelector("#addClass").value;
     let name = document.querySelector("#name"). value;
     let age = document.querySelector("#age").value;
     let city = document.querySelector("#city").value;
     let grade = document.querySelector("#grade").value;
-    axios.post(`http://localhost:3000/enrollStudent/:${addClass}`, {name, age, city, grade});
-    addClass.innerHTML = " "
-    name.innerHTML = " "
-    age.innerHTML = ""
-    city.innerHTML = ""
-    grade.innerHTML = ""
+    try {
+        axios.post(`http://localhost:3000/class/:${addClass}/enroll`, {name, age, city, grade});
+        addClass.innerHTML = " ";
+        name.innerHTML = " ";
+        age.innerHTML = " ";
+        city.innerHTML = " ";
+        grade.innerHTML = " ";
+    } catch(err) {
+        console.log(err)
+    }
 }
 
-const listStudent = async () => {
+const listStudent = async (event) => {
+    event.preventDefault();
     let sclass = document.querySelector("#sclass").value;
     let scity = document.querySelector("#scity").value;
     try {
-        let res = await axios.get("http://localhost:3000/listStudents")
+        let res = await axios.get(`http://localhost:3000/class/:${sclass}/students`);
 
     } catch (err) {
         console.log(err)
     }
 }
+
+classFrm.addEventListener("submit", addClass);
+enrollFrm.addEventListener("submit", enrollStudent);
+searchFrm.addEventListener("submit", listStudent);
