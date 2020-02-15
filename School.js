02af3bb794ca43adca62012d2a69b9e14d31598b
Kelvin <kelvinarellano@pursuit.org>
2020-02-15 11:10:43 -0500

    compleate school.js

--- School.js
+++ School.js
@@ -4,66 +4,53 @@ const Student = require('./Student')
 class School {
   constructor() {
     this.classes = {
-      // className: Class Object
-      //   physics: {} 
+      physics:{
+        name:"Physics",
+        teacher:"Mr.Winks",
+        students:[]
+      }
     }
   }
 
-  /**
-   * Add class to classes
-   * 
-   * @param {string} name - Name of the class
-   * @param {string} teacher - Name of instructor 
-   * @return {Class} Class object
-   */
   addClass(name, teacher) {
     let newClass = new Class(name, teacher);
     this.classes[name] = newClass;
+    return newClass
   }
 
-  /**
-   * Enroll student in class
-   * 
-   * @param {string} className - Name of the class
-   * @param {Student} student - Student object
-   * @return {Student} Enrolled student
-   */
   enrollStudent(className, student) {
-    // Your code here
+   let newStudent = new Student (student.name, student.age, student.city, student.grade)
+   this.classes[className]["students"].push(newStudent)
+   return newStudent
   }
 
-
-
-
-  /**
-   * Get all students enrolled in a class
-   * 
-   * @param {string} className - Name of the class
-   * @return {Student[]} Array of Student objects
-   */
   getStudentsByClass(className) {
-    // Your code here
+    return this.classes[className]["students"]
   }
 
-
-
-
-  /**
-   * Get all students and apply filters. If failing = true
-   * return all students that are failing the class, 
-   * that is all students whose grade is less than 70.
-   * If a city is passed return students whose city match
-   * the city passed. If both failing and city are passed
-   * return students that are failing and that live in the
-   * specified city
-   * 
-   * @param {string} className - Name of the class
-   * @param {boolean} failing - Whether to return students that are failing the class or not
-   * @param {string} city - Name of the city to match against students
-   * @return {Student[]} Array of Student objects
-   */
   getStudentsByClassWithFilter(className, failing, city) {
-    // Your code here
+    let filterArr = [];
+    let studentArray = this.classes[`${className}`]["students"]
+    if(failing === 'true' && city != ""){
+      for(let i = 0; i < studentArray.length; i++){
+        if(studentArray[i]["city"] === city && studentArray[i]["grade"] < 70){
+          filterArr.push(studentArray[i]);
+        }
+      }
+    } else if(failing === 'true' && city === ""){
+      for(let i = 0; i < studentArray.length; i++){
+        if(studentArray[i]["grade"] < 70){
+          filterArr.push(studentArray[i])
+          }
+        }
+    } else if(failing != 'true' && city != "") {
+      for(let i = 0; i < studentArray.length; i++){
+        if(studentArray[i]["city"] === city){
+          filterArr.push(studentArray[i])
+        }
+      }
+    }
+    return filterArr
   }
 
 }
