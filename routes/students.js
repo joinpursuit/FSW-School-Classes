b37a558bbe9cd006e7df79e247136cd2eb55b941
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-15 05:11:26 -0500

    cleaning up

--- routes/students.js
+++
@@ -1,8 +0,0 @@
-const students = require("express").Router()
-const {} = require("../routes/classes")
-
-students.get("/")
-students.get("/:id/classes")
-students.post("/", newStudent)
-
-module.exports = students;
