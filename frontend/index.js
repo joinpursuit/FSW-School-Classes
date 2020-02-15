83ea8b0bd55bd61ceae7b2011773404c12d5fab0
Syn Perez <perezsyn@gmail.com>
2020-02-15 10:14:51 -0500

    update frontend js , fix some extras

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    let ClassDiv = document.querySelector("#newClassDiv")
+    let ClassForm = document.querySelector("#newClassForm")
+    let StudentDiv = document.querySelector("#newStudentDiv")
+    let StudentForm = document.querySelector("#newStudentForm")
+    let SortDiv = document.querySelector("#sortDiv")
+    let SortForm = document.querySelector("#sortForm")
     console.log("pie");
+
+    ClassForm.addEventListener("submit",()=>{
+
+    })
+
+    StudentForm.addEventListener("submit",()=>{
+        
+    })
+
+    SortForm.addEventListener("submit",()=>{
+        
+    })
     
 })
