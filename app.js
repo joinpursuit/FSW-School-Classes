26469c91d20d5e53fd524bbb2f28b6e5795ad605
Wilsantos1975 <faustosantos@pursuit.org>
2020-03-02 11:25:15 -0500

    create post routes

--- app.js
+++ app.js
@@ -4,29 +4,63 @@ const time = require("express-timestamp");
 const bodyParser = require("body-parser");
 const port = 3000;
 const app = express();
-const School = require('./School');
+const School = require("./School");
 
 let colegio = new School();
-console.log(colegio)
+// console.log(colegio);
 //import school
 //create new instance of school
 
-
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/colegio", (req,res) => {
-    res.json(colegio.classes)
-})
+app.get("/colegio", (req, res) => {
+  res.json(colegio.classes);
+});
+
+app.post("/colegio/class", (req, res) => {
+  try {
+    const { name, teacher } = req.body;
+    // console.log(req.body);
+    colegio.addClass(name, teacher);
+    res.json({
+      class: { name: name, teacher: teacher, students: [] },
+      message: "new class created",
+      timestamp: new Date().toString()
+    });
+  } catch (error) {
+    res.json({
+      error: "Please fill out all the information or Class already exists",
+      timestamp: new Date().toString()
+    });
+  }
+});
+
+app.post("/colegio/:className/enrollStudent", (req, res) => {
+  try {
+    const { name, age, city, grade } = req.body;
+    colegio.enrollStudent(name, age, city, grade);
+    console.log(req.body);
+    res.json({
+      student: { name: name, age: age, city: city, grade: grade },
+      className: req.params.className,
+      message: "student enrolled",
+      timestamp: new Date().toString()
+    });
+  } catch (error) {
+    res.json({
+      error:
+        err + "Please fill out all the information or Class already exists",
+      timestamp: new Date().toString()
+    });
+  }
+});
 
 //create all routes and test them by using console.log
 
 //start adding functionality to each route
 
-
-
-
 app.listen(port, () => {
-    console.log("listening to port", port);
-})
+  console.log("listening to port", port);
+});
