358975310dc405f9abe627f62d0ddf0a23d9f811
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-03-02 11:57:06 -0500

    dummy data fix

--- backend/models/School.js
+++ backend/models/School.js
@@ -1,20 +1,109 @@
 const Class = require('../models/Class');
 const Student = require('../models/Student')
 
-
 class School {
   constructor() {
-    this.classes = 
-    {
-      Transfigurations: {
-        name: 'Transfigurations',
-        teacher: 'Ms. Minerva McGonagall',
-        students:[{name: "Harry Potter", age: 22, city: "Godric Hallows", grade: 86.5, house: "Gryffindor"}, {name: "Penelope Clearwater", age: 23, city: "Oxford", grade: 79.0, house: "Ravenclaw"}, {name: "Ronald Weasley", age: 22, city: "Newcastle", grade: 88.5, house: "Gryffindor"},  {name: "Susan Bones", age: 23, city: "London", grade: 89.0, house: "Hufflepuff"}]
-      }
-    } 
-  };
+    this.classes = {
+      // className: Class Object
+      //   physics: {} 
+    }
+  }
 
-  class 
+
+// class School {
+//   constructor() {
+// this.classes = {
+//   Transfigurations: 
+//         {name: 'Transfigurations',
+//         teacher: 'Ms. Minerva McGonagall',
+//         students:[{name: "Harry Potter", age: 22, city: "Godric Hallows", grade: 86.5, house: "Gryffindor"}, {name: "Penelope Clearwater", age: 23, city: "Oxford", grade: 79.0, house: "Ravenclaw"}, {name: "Ronald Weasley", age: 22, city: "Newcastle", grade: 88.5, house: "Gryffindor"},  {name: "Susan Bones", age: 23, city: "London", grade: 89.0, house: "Hufflepuff"}]}
+//     }
+//   }
+
+
+  // class Student {
+  //   constructor(name, age, city, grade) {
+  //     this.name = name
+  //     this.city = city
+  //     this.age = age
+  //     this.grade = grade
+        //  this.house = house
+  //   }
+  // }
+  
+
+
+  // this.classes = {
+  //   className: 
+  //         {name: 'Transfigurations',
+  //         teacher: 'Ms. Minerva McGonagall',
+  //         students:[{name: "Harry Potter", age: 22, city: "Godric Hallows", grade: 86.5, house: "Gryffindor"}, {name: "Penelope Clearwater", age: 23, city: "Oxford", grade: 79.0, house: "Ravenclaw"}, {name: "Ronald Weasley", age: 22, city: "Newcastle", grade: 88.5, house: "Gryffindor"},  {name: "Susan Bones", age: 23, city: "London", grade: 89.0, house: "Hufflepuff"}]}
+  //     }
+
+
+
+
+
+
+  //   {
+  //     Charms: 
+  //         {name: 'Charms',
+  //         teacher: 'Mr. Filius Flitwick',
+  //         students:[{name: "Millicent Bulstrode", age: 23, city: "Birmingham", grade: 81.0, house: "Slytherin"}, {name: "Terry Boot", age: 24, city: "Manchester", grade: 65.1, house: "Ravenclaw"}, {name: "Zacharias Smith", age: 23, city: "Chester", grade: 86.5, house: "Gryffindor"}]
+  //         }
+  
+  // }
+
+
+  //   this.classes = {
+  //     { Transfigurations:  
+  //       {
+  //       name: 'Transfigurations',
+  //       teacher: 'Ms. Minerva McGonagall',
+  //       students:[{name: "Harry Potter", age: 22, city: "Godric Hallows", grade: 86.5, house: "Gryffindor"}, {name: "Penelope Clearwater", age: 23, city: "Oxford", grade: 79.0, house: "Ravenclaw"}, {name: "Ronald Weasley", age: 22, city: "Newcastle", grade: 88.5, house: "Gryffindor"},  {name: "Susan Bones", age: 23, city: "London", grade: 89.0, house: "Hufflepuff"}]
+  //     }
+  //   }, 
+
+  //   {
+  //     Charms: 
+  //     {
+  //       name: 'Charms',
+  //       teacher: 'Mr. Filius Flitwick',
+  //       students:[{name: "Millicent Bulstrode", age: 23, city: "Birmingham", grade: 81.0, house: "Slytherin"}, {name: "Terry Boot", age: 24, city: "Manchester", grade: 65.1, house: "Ravenclaw"}, {name: "Zacharias Smith", age: 23, city: "Chester", grade: 86.5, house: "Gryffindor"}]
+  //     }
+  //   },
+
+  //   {
+  //     DefenseAgainstTheDarkArts:
+  //     {
+  //       name: 'Defense Against the Dark Arts',
+  //       teacher: 'Mr. Remus Lupin',
+  //       students:[{name: "Justin Finch-Fletchley", age: 23, city: "Croydon", grade: 68.5, house: "Hufflepuff"}, {name: "Chidera Manke", age: 23, city: "Nottingham", grade: 88.5, house: "Gryffindor"}, {name: "Hermione Granger", age: 22, city: "Liverpool", grade: 97.9, house: "Gryffindor"}, {name: "Padma Patil", age: 23, city: "Exeter", grade: 81.7, house: "Ravenclaw"}]
+  //     }
+  //   },
+
+  //   {
+  //     Potions: 
+  //     {
+  //       name: 'Potions',
+  //       teacher: 'Mr. Severus Snape',
+  //       students:[{name: "Itoro Uko", age: 25, city: "Houston", grade: 98.1, house: "Gryffindor"}, {name: "Gregory Goyle", age: 24, city: "Cambridge", grade: 61.3, house: "Slytherin"}, {name: "Lisa Turpin", age: 23, city: "Bristol", grade: 90.2, house: "Ravenclaw"}]
+  //     }
+  //   },
+
+  //   {
+  //     Herbology:
+  //     {
+  //       name: 'Herbology',
+  //       teacher: 'Ms. Pomona Sprout',
+  //       students:[{name: "Luna Lovegood", age: 22, city: "Wembley", grade: 80.0, house: "Ravenclaw"}, {name: "Draco Malfoy", age: 22, city: "Winchester", grade: 64, house: "Slytherin"}, {name: "Hannah Abbott", age: 23, city: "Wembley", grade: 61.9, house: "Hufflepuff"}]
+  //     }
+  //   }
+
+  
+  //   }
+  // };
+
 
   /**
    * Add class to classes
