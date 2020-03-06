8c28d73bed3eb05d314f64958187ffabbc516c41
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-03-06 00:11:47 -0500

    fixed bugs and displays, everything working

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // console.log("hello world")
     let select = document.querySelector("#populateClasses");
-
     const populateClasses = async () => {
             let res = await axios.get("http://localhost:3000/class/");
             let displayClasses = Object.keys(res.data.class);
@@ -11,20 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
                 select.appendChild(option)
             })
         }
-
     // in = index
     // of = value
-    
     const getAllStudentsInClass = async (className) => {
         try {
 
             let studentsByClassDiv = document.querySelector("#studentsByClassDiv")
             let res = await axios.get(`http://localhost:3000/class/${className}`);
-            debugger
             let grabClass = res.data.allStudents;
                 grabClass.forEach((showStudent) => {
                 let {name, city, age, grade, house} = showStudent
-                const listStudent = `Name: ${name} City: ${city} Age: ${age} Grade: ${grade} House: ${house}`
+                const listStudent = `Class: ${className} Name: ${name} Age: ${age} City: ${city} Grade: ${grade} House: ${house}`
                 let p1 = document.createElement("p");
                 p1.innerText = listStudent;
                 studentsByClassDiv.appendChild(p1)
@@ -33,43 +28,18 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(error)
     }
 }
-
 populateClasses()
 select.addEventListener("change", (event) => {
-    document.querySelector("#studentsByClassDiv").innerHTML = getAllStudentsInClass(event.target.value)
+   getAllStudentsInClass(event.target.value)
   
 })
-
-
-let mainDiv = document.querySelector("#mainDiv")
-// let populateClasses = document.querySelector("#populateClasses")
-// let populateStudents = document.querySelector("#populateStudents")
-let addClassDivHead = document.querySelector("#addClassDivHead")
-let addClassDivMain = document.querySelector("#addClassDivMain")
-let className = document.querySelector("#className")
-let teacherName = document.querySelector("#teacherName")
-let addClassBtn = document.querySelector("#addClassBtn")
-let newClassDisplay = document.querySelector("#newClassDisplay")
-let newClassDisplayUL = document.querySelector("#newClassDisplayUL")
-let enrollStudentDiv = document.querySelector("#enrollStudentDiv")
-let addClassForm = document.querySelector("#addClassForm")
-let enrollStudentForm = document.querySelector("#enrollStudentForm")
-let sClassName = document.querySelector("#sClassName")
-let studentName = document.querySelector("#studentName")
-let studentAge = document.querySelector("#studentAge")
-let studentCity = document.querySelector("#studentCity")
-let studentGrade = document.querySelector("#studentGrade")
-let studenetEnrollBtn = document.querySelector("#studenetEnrollBtn")
-let newStudentDisplay = document.querySelector("#newStudentDisplay")
-let newStudentDisplayUL = document.querySelector("#newStudentDisplayUL")
-let failingForm = document.querySelector("#failingForm")
-let enterClass = document.querySelector("#enterClass")
-let addCity = document.querySelector("#addCity")
-let displayFailing = document.querySelector("#displayFailing")
-let fsBtn = document.querySelector("#fsBtn")
-let failingStudentList = document.querySelector("#failingStudentList")
-let failingStudentListUL = document.querySelector("#failingStudentListUL")
-
+    let newClassDisplay = document.querySelector("#newClassDisplay")
+    let newClassDisplayUL = document.querySelector("#newClassDisplayUL")
+    let addClassForm = document.querySelector("#addClassForm")
+    let enrollStudentForm = document.querySelector("#enrollStudentForm")
+    let newStudentDisplay = document.querySelector("#newStudentDisplay")
+    let newStudentDisplayUL = document.querySelector("#newStudentDisplayUL")
+    let failingForm = document.querySelector("#failingForm")
 
 
 
@@ -85,11 +55,9 @@ try {
     newClassDisplay.appendChild(h3)
 } catch (error) {
     console.log(error)
-
 } 
   })
-
-  enrollStudentForm.addEventListener("submit", async (event) => {
+enrollStudentForm.addEventListener("submit", async (event) => {
     event.preventDefault()
     let sClassName = document.querySelector("#sClassName").value
     let name = document.querySelector("#studentName").value
@@ -107,8 +75,44 @@ try {
     console.log(error)
 }
   })
-
-})
-
+failingForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        let failingStudentList = document.querySelector("#failingStudentList")
+        let allStudentsList = document.querySelector("#allStudentsList")
+        let enterClass = document.querySelector("#enterClass").value
+        let grabFailing = document.querySelector("#grabFailing").checked
+        let addCity = document.querySelector("#addCity").value;
     
-    
+        if(grabFailing === true && addCity) {
+            let res = await axios.get(`http://localhost:3000/class/${enterClass}/students?failing=true`)
+            let failingStudents = res.data.students
+            failingStudents.forEach((failedStudent) => {
+            let {name, city, age, grade, house} = failedStudent
+            const listStudent = `From the city of ${city}, ${age} year old ${name} has failed the ${enterClass} class with an overall grade of a ${grade}. They belong to the ${house} house.`
+            let p2 = document.createElement("p");
+            p2.innerText = listStudent;
+            failingStudentList.appendChild(p2)
+            })
+        } else if (grabFailing){
+            let res = await axios.get(`http://localhost:3000/class/${enterClass}/students?failing=true`)
+            let failingStudents = res.data.students;
+            failingStudents.forEach((failedStudent) => {
+            let {name, age, city, grade, house} = failedStudent
+            const showList = `From the ${house} house, ${age} year old ${name} has failed the ${enterClass} class with an overall grade of a ${grade}.`
+            let p3 = document.createElement("p");
+            p3.innerText = showList;
+            failingStudentList.appendChild(p3)
+            })
+        } else {
+            let res = await axios.get(`http://localhost:3000/class/${enterClass}/students?failing=false`)      
+            let allStudents = res.data.students;
+            allStudents.forEach((students) => {
+            let {name, age, city, grade, house} = students
+            const listAllStudents = `There are no failing students in this class but you are welcomed to view it yourself. Name: ${name} Age: ${age} City: ${city} Grade: ${grade} House: ${house}`
+            let p4 = document.createElement("p");
+            p4.innerText = listAllStudents;
+            allStudentsList.appendChild(p4)
+            })
+        }  
+    })
+}) 
