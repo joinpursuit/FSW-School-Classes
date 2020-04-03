afdb5b799d5486c5f45d1fe46fc70fc16ff45fb5
DejaLFlynn <dejaflynn@pursuit.org>
2020-04-03 17:48:07 -0400

    corrected backend values in add class

--- app.js
+++ app.js
@@ -1,131 +1,134 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000;
 const School = require("./School");
-const Class = require('./Class');
-const Student = require('./Student')
-
-
+const Class = require("./Class");
+const Student = require("./Student");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 let mySchool = new School();
-const allClasses = (request,response)=>{
-response.status(200).json({
+const allClasses = (request, response) => {
+  response.status(200).json({
     status: "success",
     message: "All classes",
-    body: mySchool.classes
-})
-}
-const checkClassName = (request,response,next)=>{
-    let classes = request.body.classes
-    let teacher = request.body.teacher
-if(!classes||!teacher){
+    body: mySchool.class
+  });
+};
+const checkClassName = (request, response, next) => {
+  console.log("checkClassName");
+  let name = request.body.name;
+  let teacher = request.body.teacher;
+  if (!name || !teacher) {
     response.json({
-        error:"not valid data",
-        time: new Date()
-    })
-}else{
-    next()
-}
-}
-const addClass =(request,response)=>{
-    let classes = request.body.classes
-    let teacher = request.body.teacher
-    let newClass = mySchool.addClass(classes, teacher)
-    request.status(200).json({
-        status:"success",
-        message: "added a new class",
-        body: newClass,
-        time: new Date()
-    })
-
-}
-const ifRepeated =(request,response,next)=>{
-let className = request.body.name
-if(!mySchool.classes[className]){
+      error: "not valid data",
+      time: new Date()
+    });
+  } else {
+    next();
+  }
+};
+const addClass = (request, response) => {
+  console.log("addClass");
+  let name = request.body.name;
+  let teacher = request.body.teacher;
+  let newClass = mySchool.addClass(name, teacher);
+  response.status(200).json({
+    status: "success",
+    message: "added a new class",
+    body: newClass,
+    time: new Date()
+  });
+};
+const ifRepeated = (request, response, next) => {
+  console.log("ifRepeated");
+  let className = request.body.name;
+  if (!mySchool.classes[className]) {
     response.json({
-        error:"class exists",
-        time: new Date()
-    })
-}
-next()
+      error: "class exists",
+      time: new Date()
+    });
+  }
+  next();
+};
+const enrollStudent = (request, response) => {
+  let name = request.body.name;
+  let age = request.body.age;
+  let city = request.body.city;
+  let grade = request.body.grade;
+  let className = request.params.name;
+  let newStudent = new Student(name, age, city, grade, className);
+  let enrolledStudent = mySchool.enrollStudent(
+    request.params["className"],
+    newStudent
+  );
+  if (!enrolledStudent) {
+    response.json({
+      error: "student does not exist",
+      time: new Date()
+    });
+  } else {
+    response.json({
+      student: enrolledStudent,
+      class: className,
+      message: "Enrolled ",
+      time: new Date()
+    });
+  }
+};
+const classExist = (request, response, next) => {
+  let className = request.params.classes;
+  if (!mySchool.classes[className]) {
+    response.json({});
+  } else {
+    next();
+  }
+};
+const studentExist = (request, response, next) => {
+  if (
+    !request.body.name ||
+    !request.body.age ||
+    !request.body.city ||
+    !request.body.grade
+  ) {
+    response.json({
+      error: "student exists",
+      time: new Date()
+    });
+  } else {
+    next();
+  }
+};
+const studentBody = (request, response) => {
+  let className = request.body.classes;
+  let failing = request.query.failing;
+  let city = request.query.city;
+  if (failing === "true") {
+    failing = true;
+  } else {
+    failing = false;
+  }
+  let filtered = [];
+  if (!failing && !city) {
+    filtered = mySchool.getStudentsByClass(className);
+  } else {
+    response.json({
+      students: filtered,
+      message: "Retrieved Students",
+      timestamp: new Date()
+    });
+  }
+};
 
-}
-const enrollStudent=(request,response)=>{
-    let name = request.body.name;
-    let age = request.body.age;
-    let city = request.body.city;
-    let grade = request.body.grade;
-    let className = request.params.name;
-    let newStudent = new Student(name, age, city, grade, className)
-    let enrolledStudent = mySchool.enrollStudent(request.params['className'], newStudent);
-    if(!enrolledStudent){
-        response.json({
-            error: "student does not exist",
-            time: new Date()
-        })
-    }else {
-        response.json({
-            student : enrolledStudent,
-            class: className,
-            message: 'Enrolled ',
-            time: new Date()
-        })
-    }
-}
-const classExist=(request,response,next)=>{
-    let className = (request.params.name)
-    if(!mySchool.classes[className]){
-        response.json({
+app.get("/", allClasses);
+app.post("/class", checkClassName, addClass, ifRepeated);
+app.post("/class/:className", classExist, enrollStudent, studentExist);
+app.get("/class/:className/students/failing", classExist, studentBody);
 
-        })
-    }else{
-        next()
-    }
-}
-const studentExist=(request,response,next)=>{
-    if(!request.body.name||!request.body.age|| !request.body.city|| !request.body.grade){
-        response.json({
-            error: "student exists",
-            time: new Date()
-        })
-    }else{
-        next()
-    }
-
-}
-const studentBody=(request,response)=>{
-    let className = request.body.name;
-    let failing = request.query.failing;
-    let city = request.query.city;
-    if (failing === 'true') {
-        failing = true;
-    } else {
-        failing = false;
-    }
-    let filtered = [];
-    if (!failing && !city) {
-        filtered = mySchool.getStudentsByClass(className);
-    } else {
-        response.json({
-            students: filtered,
-            message: "Retrieved Students",
-            timestamp: new Date()
-        }) 
-
-    }
-}
-
-app.get("/", allClasses)
-app.post("/class",checkClassName,addClass,ifRepeated)
-app.post("/class/:className",classExist,enrollStudent,studentExist)
-app.get("/class/:className/students/failing",classExist,studentBody)
-
-app.listen(port,()=>{
-    console.log("server is running", port)
-})
+app.listen(port, () => {
+  console.log("server is running", port);
+});
--- index.js
+++ index.js
@@ -1,79 +1,82 @@
-let classAdd = document.querySelector("#classAdd")
-let addClass = document.querySelector("#addClass")
-let studentEnroll = document.querySelector("#studentEnroll")
-let addStudent = document.querySelector("#addStudent")
-let list = document.querySelector("#list")
-let listStudents = document.querySelector("#listStudents")
-addClass.addEventListener("submit",async (e)=>{
-    e.preventDefault()
-            let classes = document.querySelector("#className")
-            let teacher = document.querySelector("#teacherName")
-            let classesInput = classes.value
-            let teacherInput = teacher.value
-            classesInput.value = ""
-            teacherInput.value = ""
-            let p = document.createElement("p")
-            try{
+let classAdd = document.querySelector("#classAdd");
+let addClass = document.querySelector("#addClass");
+let studentEnroll = document.querySelector("#studentEnroll");
+let addStudent = document.querySelector("#addStudent");
+let list = document.querySelector("#list");
+let listStudents = document.querySelector("#listStudents");
+addClass.addEventListener("submit", async e => {
+  e.preventDefault();
+  let name = document.querySelector("#className").value;
+  let teacher = document.querySelector("#teacherName").value;
+  let p = document.createElement("p");
+  let h4 = document.createElement("h4")
+  try {
+    let host = await axios.post(`http://localhost:4000/class`, {
+      teacher: teacher,
+      name: name
+    });
+    debugger;
+    // host.data.body.name
 
-                let host = await axios.post(`http://localhost:4000/class`, {teacher: teacherInput, class: classesInput})
-                debugger
-                p.innerText = host.data
-                classAdd.appendChild(p)
-            }catch(error){
-                
-            }
-      
+    h4.innerText = host.data.body.name;
+    p.innerText = host.data.body.teacher;
     
-
-})
-addStudent.addEventListener("submit",async(e)=>{
-    e.preventDefault()
-        let className = document.querySelector("#class").value
-        let name = document.querySelector("#student").value
-        let age = document.querySelector("#age").value
-        let city = document.querySelector("#city").value
-        let grade = document.querySelector("#grade").value
-        className.value = ""
-        name.value = ""
-        age.value = ""
-        grade.value = ""
-        city.value = ""
-        let p = document.createElement("p")
-        try{
-
-            let host = await axios.post(`http://localhost:4000/class/${className}`, {class: className, name, age, grade, city})
-            debugger
-            p.innerText = host.data
-            studentEnroll.appendChild(p)
-        }catch(error){
-            console.log(error)
-        }
-  
-})
-listStudents.addEventListener("submit",async(e)=>{
-    e.preventDefault()
-    let className = document.querySelector("#classList").value
-    let city = document.querySelector("#cityList").value
-    className.value = ""
-    city.value =""
-    let failing = document.querySelector("#fail")
-    let ul = document.querySelector("ul")
-    let li = document.createElement("li")
-    if (failing.checked) {
-        failing = 'true';
-    } else {
-        failing = 'false';
-    }
-    try{
-        
-        let host = await axios.post(`http://localhost:4000/class/${className}/students?failing=${failing}&city=${city}`, {className:className,failing:failing})
-        debugger
-        host.forEach(el=>{
-        li.innerText = el.name + el.age + el.city + el.grade
-        ul.appendChild(li)
-        })
-    }catch(error){
-        console.log(error)
-    }
-})
-
+    classAdd.appendChild(p);
+  } catch (error) {}
+});
+addStudent.addEventListener("submit", async e => {
+  e.preventDefault();
+  let className = document.querySelector("#class").value;
+  let name = document.querySelector("#student").value;
+  let age = document.querySelector("#age").value;
+  let city = document.querySelector("#city").value;
+  let grade = document.querySelector("#grade").value;
+  className.value = "";
+  name.value = "";
+  age.value = "";
+  grade.value = "";
+  city.value = "";
+  let p = document.createElement("p");
+  try {
+    let host = await axios.post(`http://localhost:4000/class/${className}`, {
+      class: className,
+      name,
+      age,
+      grade,
+      city
+    });
+    debugger;
+    p.innerText = host.data;
+    studentEnroll.appendChild(p);
+  } catch (error) {
+    console.log(error);
+  }
+});
+listStudents.addEventListener("submit", async e => {
+  e.preventDefault();
+  let className = document.querySelector("#classList").value;
+  let city = document.querySelector("#cityList").value;
+  className.value = "";
+  city.value = "";
+  let failing = document.querySelector("#fail");
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li");
+  if (failing.checked) {
+    failing = "true";
+  } else {
+    failing = "false";
+  }
+  try {
+    let host = await axios.post(
+      `http://localhost:4000/class/${className}/students?failing=${failing}&city=${city}`,
+      { className: className, failing: failing }
+    );
+    debugger;
+    host.forEach(el => {
+      li.innerText = el.name + el.age + el.city + el.grade;
+      ul.appendChild(li);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
