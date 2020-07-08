5d1a1c22531773ef164c5b10b2b9c0eb5acf091f
vonbarown <vonielbrown@pursuit.org>
2020-07-08 18:11:37 -0400

    updating dom for auth functionality

--- client/auth/login/login.js
+++ client/auth/login/login.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   let studentLogin = document.querySelector("#studentLogin");
+  // let studentLoginForm = document.querySelector("#studentLoginForm");
 
+  // studentLoginForm.addEventListener("submit", (e) => {
+  //   console.log("hello");
+  // });
+  loadLoginForm();
   studentLogin.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "../../landing/index.html";
   });
-
-  console.log("hello");
 });
+
+const loadLoginForm = async () => {
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
+
+  console.log("user:", username);
+  console.log("pass:", password);
+};
 
 const clearResults = () => {
   let container = getContainer();
