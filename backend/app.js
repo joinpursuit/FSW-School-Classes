33d3157103970a4ba9ca991435462a0b289bf931
vonbarown <vonielbrown@pursuit.org>
2020-07-11 21:47:13 -0400

    update 2

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../index.html")));
 app.options("*", cors());
 
 app.use("/class", classRouter);
